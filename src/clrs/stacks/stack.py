522948b27d1ec7d9f91475570b8da026485e0dcf
zzhao30 <zzhao30@ford.com>
2021-09-22 10:41:16 -0400

    Added table doulbing to stacks.

--- src/clrs/stacks/stack.py
+++ src/clrs/stacks/stack.py
@@ -5,6 +5,8 @@ We can implement a stack of at most n elements with an array, S, of size n. The 
 has an attribute S.top that indexes the most recently inserted element. When S.top = -1,
 the stack contains no elements and is empty. When S.top == n, then the stack overflows.
 
+NB: Stack mimics table doubling for practice even though it's redundant in Python.
+
 Each of the stack operations take O(1).
 """
 
@@ -15,15 +17,74 @@ class Stack:
         self.a = [None] * self.size
         self.top = -1
 
+    def _grow(self):
+        if self.top + 1 == self.size:
+            self.a = self.a + [None] * self.size
+            self.size *= 2
+
+    def _reduce(self):
+        if self.top + 1 == self.size // 4:
+            self.a = [self.a[i] for i in range(self.size // 2)]
+            self.size = int(self.size / 2)
+
     def empty(self):
         return self.top == -1
 
     def pop(self):
         assert not self.empty()
         self.top -= 1
+        self._reduce()
         return self.a[self.top + 1]
 
     def push(self, x):
-        assert self.top + 1 != self.size
+        self._grow()
         self.top += 1
         self.a[self.top] = x
+
+
+x = Stack(2)
+x.push(1)
+print(x.a)
+x.push(2)
+print(x.a)
+x.push(3)
+print(x.a)
+x.push(4)
+print(x.a)
+x.push(5)
+print(x.a)
+x.push(6)
+print(x.a)
+x.push(7)
+print(x.a)
+x.push(8)
+print(x.a)
+x.push(9)
+print(x.a)
+print()
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+print(x.pop())
+print(x.a)
+x.push(666)
+print(x.a)
+x.push(667)
+print(x.a)
+x.push(668)
+print(x.a)
+x.push(669)
+print(x.a)
--- tests/clrs/stacks/test_stack.py
+++ tests/clrs/stacks/test_stack.py
@@ -12,3 +12,51 @@ def test_stack():
     for i in range(size):
         assert stack.pop() == item[size - i - 1]
     assert stack.empty()
+
+
+def test_table_doubling():
+    x = Stack(2)
+    x.push(1)
+    assert x.a == [1, None]
+    x.push(2)
+    assert x.a == [1, 2]
+    x.push(3)
+    assert x.a == [1, 2, 3, None]
+    x.push(4)
+    assert x.a == [1, 2, 3, 4]
+    x.push(5)
+    assert x.a == [1, 2, 3, 4, 5, None, None, None]
+    x.push(6)
+    assert x.a == [1, 2, 3, 4, 5, 6, None, None]
+    x.push(7)
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, None]
+    x.push(8)
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8]
+    x.push(9)
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None]
+    assert x.pop() == 9
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None]
+    assert x.pop() == 8
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None]
+    assert x.pop() == 7
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None]
+    assert x.pop() == 6
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8, 9, None, None, None, None, None, None, None]
+    assert x.pop() == 5
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert x.pop() == 4
+    assert x.a == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert x.pop() == 3
+    assert x.a == [1, 2, 3, 4]
+    assert x.pop() == 2
+    assert x.a == [1, 2]
+    assert x.pop() == 1
+    assert x.a == [1]
+    x.push(666)
+    assert x.a == [666]
+    x.push(667)
+    assert x.a == [666, 667]
+    x.push(668)
+    assert x.a == [666, 667, 668, None]
+    x.push(669)
+    assert x.a == [666, 667, 668, 669]
