4a3f3499783bef351a2cf9153c338e5c4c839887
FruVirus <zhen.zhao6@gmail.com>
2021-09-25 16:11:30 -0400

    CCs.

--- src/clrs/string_matching/naive.py
+++ src/clrs/string_matching/naive.py
@@ -1,4 +1,8 @@
-"""The naive algorithm finds all valid shifts using a loop that checks the condition
+"""
+Overview
+========
+
+The naive algorithm finds all valid shifts using a loop that checks the condition
 P[1...m] = T[s + 1...s + m] for each of the n - m + 1 possible values of s, where P is
 the pattern to find, T is the text from which to find the pattern, and s is the shift
 value where the pattern occurs.
@@ -6,7 +10,10 @@ value where the pattern occurs.
 Because the naive algorithm requires no preprocessing, its running time equals its
 matching time.
 
-Naive: O((n - m + 1) * m), where n is the length of the text and m is the length of the
+Complexity
+==========
+
+O((n - m + 1) * m), where n is the length of the text and m is the length of the
 pattern.
 """
 
