c96148f9cd7fe9adca5469a3b340f045d42db01d
FruVirus <zhen.zhao6@gmail.com>
2021-09-25 21:38:34 -0400

    CCs.

--- src/clrs/sorting/linear_sorting/counting_sort.py
+++ src/clrs/sorting/linear_sorting/counting_sort.py
@@ -1,5 +1,9 @@
-"""Counting sort assumes that each of the n input elements is an integer in the range 0
-to k, for some integer k. When k = O(n), the sort runs in Theta(n) time.
+"""
+Overview
+========
+
+Counting sort assumes that each of the n input elements is an integer in the range 0 to
+k, for some integer k. When k = O(n), the sort runs in Theta(n) time.
 
 Counting sort determines, for each input element x, the number of elements less than x.
 It uses this information to place element x directly into its position in the output
@@ -13,6 +17,19 @@ when satellite data are carried around with the element being sorted.
 
 Counting sort's stability is the reason why it's also used as a subroutine in radix
 sort.
+
+NB: For radix sort, counting sort makes use of the exp and base arguments and
+incrementally sorts by least significant digit.  If counting_sort() is used to sort a
+given array with provided exp and base arguments, it will NOT always sort correctly
+since counting_sort() is stable. For example, sorting the array
+[329, 457, 657, 839, 436, 720, 355] with exp = 100 and base 10 will result in
+[329, 355, 457, 436, 657, 720, 839] since 457 came before 436 in the original array and
+their most significant digit is the same (i.e, 4) and counting_sort is a stable sorting
+algorithm. In other words, counting_sort() only sorts properly if it is iteratively
+called with increasing significant digit.
+
+Complexity
+==========
 
 Stable
 O(n)
