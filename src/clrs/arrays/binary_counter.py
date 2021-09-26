d5cb7f885da409c6ce2ed47a552f0e3411a081ce
FruVirus <zhen.zhao6@gmail.com>
2021-09-25 21:13:52 -0400

    CCs.

--- src/clrs/arrays/binary_counter.py
+++ src/clrs/arrays/binary_counter.py
@@ -1,7 +1,14 @@
-"""Binary counter implements a k-bit binary counter that counts upwards from 0. We use
-an array A[0...k - 1] of bits, where len(A) = k, as the counter. A binary number x that
-is stored in the counter has its lowest-order bit in A[0] and its highest-order bit in
+"""
+Overview
+========
+
+Binary counter implements a k-bit binary counter that counts upwards from 0. We use an
+array A[0...k - 1] of bits, where len(A) = k, as the counter. A binary number x that is
+stored in the counter has its lowest-order bit in A[0] and its highest-order bit in
 A[k - 1].
+
+Complexity
+==========
 
 Theta(k)
 """
