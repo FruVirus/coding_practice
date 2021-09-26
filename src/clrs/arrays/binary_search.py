e894179cbe5651821cb401e6d1a1fe2863f5df54
FruVirus <zhen.zhao6@gmail.com>
2021-09-25 21:15:35 -0400

    CCs.

--- src/clrs/arrays/binary_search.py
+++ src/clrs/arrays/binary_search.py
@@ -1,6 +1,12 @@
-"""Binary search searches for an item k in an array by recursively splitting the array
-in two. The input array is assumed to be sorted in increasing order.
+"""
+Overview
+========
 
+Binary search searches for an item k in an array by recursively splitting the array in
+two. The input array is assumed to be sorted in increasing order.
+
+Complexity
+==========
 O(lg(n))
 """
 
