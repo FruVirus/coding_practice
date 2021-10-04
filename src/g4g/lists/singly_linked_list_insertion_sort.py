e9dbbd54dd56a05a861b53fd66ca984332904ecb
zzhao30 <zzhao30@ford.com>
2021-10-04 12:55:25 -0400

    Added SLLInsertionSort.

--- src/g4g/lists/singly_linked_list_insertion_sort.py
+++ src/g4g/lists/singly_linked_list_insertion_sort.py
@@ -37,8 +37,6 @@ class SLLInsertionSort(SLL):
             next = curr.next
             sorted_ = self.insertion_sort(sorted_, curr)
             curr = next
-            print(curr.k)
-            exit()
         sorted_list = []
         while sorted_ is not None:
             sorted_list.append(sorted_.k)
