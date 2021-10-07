d8af03ca098edf6f3618008e9101b594bd6efb04
zzhao30 <zzhao30@ford.com>
2021-10-07 14:21:01 -0400

    Added rank.

--- src/clrs/matrices/rank.py
+++ src/clrs/matrices/rank.py
@@ -29,17 +29,14 @@ def rank(a):
                     for i in range(curr_rank):
                         a[col][i] -= multiplier * a[row][i]
         else:
-            reduce = True
             for i in range(row + 1, n):
                 if a[i][row] != 0:
                     swap_rows(a, row, i, curr_rank)
-                    reduce = False
                     break
-            if reduce:
+            else:
                 curr_rank -= 1
                 for i in range(n):
                     a[i][row] = a[i][curr_rank]
-            row -= 1
     return curr_rank
 
 
