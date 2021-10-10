d0137801b2d13be597cf69f5d78a759332e9126f
zzhao30 <zzhao30@ford.com>
2021-10-10 15:26:30 -0400

    CCs.

--- src/clrs/sorting/comparison_sorting/insertion_sort.py
+++ src/clrs/sorting/comparison_sorting/insertion_sort.py
@@ -24,8 +24,7 @@ O(n) if a is already sorted
 
 def insertion_sort(a):
     for i in range(1, len(a)):
-        k = a[i]
-        j = i - 1
+        j, k = i - 1, a[i]
         while j > -1 and a[j] > k:
             a[j + 1] = a[j]
             j -= 1
