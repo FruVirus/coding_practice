839744537d317321153f8317728a9e85c6070b4a
zzhao30 <zzhao30@ford.com>
2021-11-09 15:09:59 -0500

    CCs.

--- src/clrs/graph_algorithms/minimum_spanning_trees/prim.py
+++ src/clrs/graph_algorithms/minimum_spanning_trees/prim.py
@@ -44,36 +44,45 @@ class MST(Graph):
         for v in self.vertices.values():
             v.key, v.p = float("inf"), None
         self.vertices[root].key = 0
-        q = HeapQueue(list(self.vertices.keys()), False)
+        fru = [x for x in self.vertices.keys()]
+        poo = [x.key for x in self.vertices.values()]
+        print(fru)
+        print(poo)
+        q = HeapQueue(poo, False)
+        index = 0
         while q.heap_size != 0:
             u = q.extract()
-            u_node, v = self.vertices[u], self.adj_list[u].head
-            while v is not None:
-                v_node = self.vertices[v.k]
-                if v.k in q.a and self.weights[(u, v.k)] < v_node.key:
-                    v_node.key, v_node.p = self.weights[(u, v.k)], u_node
-                v = v.next
-        for v in [v for k, v in self.vertices.items() if k != root]:
-            mst.add((v.p.k, v.k))
+            u = fru[index]
+            index += 1
+            print(u)
+        #     u_node, v = self.vertices[u], self.adj_list[u].head
+        #     while v is not None:
+        #         v_node = self.vertices[v.k]
+        #         print(v_node.k)
+        #         if v.k in q.a and self.weights[(u, v.k)] < v_node.key:
+        #             v_node.key, v_node.p = self.weights[(u, v.k)], u_node
+        #         v = v.next
+        # for v in [v for k, v in self.vertices.items() if k != root]:
+        #     mst.add((v.p.k, v.k))
         return mst
 
 
 num_vertices = 9
 graph = MST(num_vertices)
-graph.add_edge(0, 1, 4)
+graph.add_edge(0, 1, 9)
 graph.add_edge(0, 7, 8)
-graph.add_edge(1, 2, 8)
-graph.add_edge(1, 7, 11)
-graph.add_edge(2, 3, 7)
-graph.add_edge(2, 5, 4)
-graph.add_edge(2, 8, 2)
-graph.add_edge(3, 4, 9)
-graph.add_edge(3, 5, 14)
-graph.add_edge(4, 5, 10)
-graph.add_edge(5, 6, 2)
-graph.add_edge(6, 7, 1)
-graph.add_edge(6, 8, 6)
-graph.add_edge(7, 8, 7)
+# graph.add_edge(1, 2, 8)
+# graph.add_edge(1, 7, 11)
+# graph.add_edge(2, 3, 7)
+# graph.add_edge(2, 5, 4)
+# graph.add_edge(2, 8, 2)
+# graph.add_edge(3, 4, 9)
+# graph.add_edge(3, 5, 14)
+# graph.add_edge(4, 5, 10)
+# graph.add_edge(5, 6, 2)
+# graph.add_edge(6, 7, 1)
+# graph.add_edge(6, 8, 6)
+# graph.add_edge(7, 8, 7)
 mst = graph.prim()
 print(mst)
 # assert mst == {(0, 1), (1, 2), (3, 4), (2, 3), (6, 7), (5, 6), (2, 5), (2, 8)}
