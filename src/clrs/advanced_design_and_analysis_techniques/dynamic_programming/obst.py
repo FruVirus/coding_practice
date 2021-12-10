3657340653a0b77166f9ea48e1a6d1316916c9eb
zzhao30 <zzhao30@ford.com>
2021-12-10 13:36:15 -0500

    CCs.

---
+++ src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/obst.py
@@ -0,0 +1,25 @@
+"""
+15.5 Optimal binary search trees
+================================
+
+Complexity
+==========
+
+Time
+----
+
+Space
+-----
+"""
+
+
+def obst_bottom_up():
+    pass
+
+
+def obst_top_down():
+    pass
+
+
+def obst_solution():
+    pass
---
+++ tests/clrs/advanced_design_and_analysis_techniques/dynamic_programming/test_obst.py
@@ -0,0 +1,6 @@
+# Repository Library
+from clrs.advanced_design_and_analysis_techniques.dynamic_programming.obst import obst
+
+
+def test_obst():
+    pass
