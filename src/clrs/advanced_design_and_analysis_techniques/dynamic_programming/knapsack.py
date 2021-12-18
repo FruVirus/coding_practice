b182826a46812ba55c4c3e51d1537c696f933af9
zzhao30 <zzhao30@ford.com>
2021-12-18 15:05:40 -0500

    CCs.

--- src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/knapsack.py
+++ src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/knapsack.py
@@ -57,18 +57,18 @@ Complexity
 Time
 ----
 
-ks_bottom_up(): O(n * c), where n is the number of items and c is the capacity.
-ks_top_down(): O(n * c), where n is the number of items and c is the capacity.
+ks_bu(): O(n * c), where n is the number of items and c is the capacity.
+ks_td(): O(n * c), where n is the number of items and c is the capacity.
 
 Space
 -----
 
-ks_bottom_up(): O(c * n) for v table.
-ks_top_down(): O(c * n) for v table.
+ks_bu(): O(c * n) for v table.
+ks_td(): O(c * n) for v table.
 """
 
 
-def ks_bottom_up(p, w, c, n, v):
+def ks_bu(p, w, c, n, v):
     for i in range(1, n + 1):
         for j in range(1, c + 1):
             loc = j - w[i - 1]
@@ -77,34 +77,30 @@ def ks_bottom_up(p, w, c, n, v):
     return v
 
 
-def ks_top_down(p, w, c, n, v):
+def ks_td(p, w, c, n, v):
     if n == 0 or c == 0:
         v[n][c] = 0
     elif w[n - 1] > c:
-        v[n][c] = ks_top_down(p, w, c, n - 1, v)
+        v[n][c] = ks_td(p, w, c, n - 1, v)
     else:
-        without_item = ks_top_down(p, w, c, n - 1, v)
-        with_item = ks_top_down(p, w, c - w[n - 1], n - 1, v) + p[n - 1]
+        without_item = ks_td(p, w, c, n - 1, v)
+        with_item = ks_td(p, w, c - w[n - 1], n - 1, v) + p[n - 1]
         v[n][c] = max(with_item, without_item)
     return v[n][c]
 
 
-def ks_solution(p, w, c, top_down=False):
+def ks(p, w, c, td=False):
     n = len(p)
     v = [[0] * (c + 1) for _ in range(n + 1)]
-    ks = ks_top_down if top_down else ks_bottom_up
+    ks = ks_td if td else ks_bu
     ks(p, w, c, n, v)
-    return v
-
-
-def return_ks(p, v):
     n = len(v)
-    ks, val = [0] * (n - 1), v[-1][-1]
+    sol, val = [0] * (n - 1), v[-1][-1]
     for i in range(n - 1, -1, -1):
         for j in range(i - 1, -1, -1):
             if val in v[j]:
-                ks[j] = 0
+                sol[j] = 0
                 break
         else:
-            ks[i - 1], val = 1, val - p[i - 1]
-    return ks
+            sol[i - 1], val = 1, val - p[i - 1]
+    return sol
