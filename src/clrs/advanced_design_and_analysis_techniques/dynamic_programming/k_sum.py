71d94b9e2ed237500f53e45c62ddc1cf16c45eab
FruVirus <zhen.zhao6@gmail.com>
2021-12-18 20:59:02 -0500

    CCs.

--- src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/k_sum.py
+++ src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/k_sum.py
@@ -28,57 +28,52 @@ Complexity
 Time
 ----
 
-ksum_bottom_up(): O(n * c * k), where n is the number of items, c is the capacity, and k
-is the number of slots.
-ksum_top_down(): O(n * c * k), where n is the number of items, c is the capacity, and k
-is the number of slots.
+ksum_bu(): O(n * c * k), where n is the number of items, c is the capacity, and k is the
+number of slots.
+ksum_td(): O(n * c * k), where n is the number of items, c is the capacity, and k is the
+number of slots.
 
 Space
 -----
 
-ksum_bottom_up(): O(c * n * k) for v table.
-ksum_top_down(): O(c * n * k) for v table.
+ksum_bu(): O(c * n * k) for v table.
+ksum_td(): O(c * n * k) for v table.
 """
 
 
-def ksum_bottom_up(w, c, n, k_, v):
+def ksum_bu(w, c, n, k, v):
     for i in range(1, n + 1):
         for j in range(1, c + 1):
-            for k in range(1, k_ + 1):
+            for k_ in range(1, k + 1):
                 loc = j - w[i - 1]
-                val = -float("inf") if loc < 0 else v[i - 1][loc][k - 1]
-                v[i][j][k] = max(v[i - 1][j][k], val)
-    return v
+                val = -float("inf") if loc < 0 else v[i - 1][loc][k_ - 1]
+                v[i][j][k_] = max(v[i - 1][j][k_], val)
 
 
-def ksum_top_down(w, c, n, k_, v):
-    if n == 0 or (c == 0 and k_ == 0):
-        v[n][c][k_] = 1 if c == 0 and k_ == 0 else 0
+def ksum_td(w, c, n, k, v):
+    if n == 0 or (c == 0 and k == 0):
+        v[n][c][k] = 1 if c == 0 and k == 0 else 0
     elif w[n - 1] > c:
-        v[n][c][k_] = ksum_top_down(w, c, n - 1, k_, v)
+        v[n][c][k] = ksum_td(w, c, n - 1, k, v)
     else:
-        without_item = ksum_top_down(w, c, n - 1, k_, v)
-        with_item = ksum_top_down(w, c - w[n - 1], n - 1, k_ - 1, v)
-        v[n][c][k_] = max(with_item, without_item)
-    return v[n][c][k_]
+        without_item = ksum_td(w, c, n - 1, k, v)
+        with_item = ksum_td(w, c - w[n - 1], n - 1, k - 1, v)
+        v[n][c][k] = max(with_item, without_item)
+    return v[n][c][k]
 
 
-def ksum_solution(w, c, k_, top_down=False):
+def ksum(w, c, k, td=False):
     n = len(w)
-    v = [[[0] * (k_ + 1) for _ in range(c + 1)] for _ in range(n + 1)]
+    v = [[[0] * (k + 1) for _ in range(c + 1)] for _ in range(n + 1)]
     for i in range(n + 1):
         v[i][0][0] = 1
-    ksum = ksum_top_down if top_down else ksum_bottom_up
-    ksum(w, c, n, k_, v)
-    return v
-
-
-def return_ksum(w, c, k_, v):
+    ksum_ = ksum_td if td else ksum_bu
+    ksum_(w, c, n, k, v)
     if v[-1][-1][-1] != 1:
         return None
     n = len(v)
-    ksum = [0] * (n - 1)
-    for i in range(n - 1, -1, -1):
-        if v[i][c][k_] != 1:
-            ksum[i], c, k_ = 1, c - w[i], k_ - 1
-    return ksum
+    sol = [0] * (n - 1)
+    for i in reversed(range(n)):
+        if v[i][c][k] != 1:
+            sol[i], c, k = 1, c - w[i], k - 1
+    return sol
