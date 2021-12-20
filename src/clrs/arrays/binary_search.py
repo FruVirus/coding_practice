dbe2fa16e42b350f5efdfdb3efad7c87c38fd353
zzhao30 <zzhao30@ford.com>
2021-12-20 13:49:38 -0500

    CCs.

--- src/clrs/arrays/binary_search.py
+++ src/clrs/arrays/binary_search.py
@@ -8,16 +8,29 @@ two. The input array is assumed to be sorted in increasing order.
 Complexity
 ==========
 
-O(lg(n))
+bs_iterative(): O(lg n)
+bs_recursive(): O(lg n)
 """
 
 
-def binary_search(a, low, high, k):
+def bs_iterative(a, low, high, k):
+    while high >= low:
+        mid = (low + high) // 2
+        if a[mid] == k:
+            return mid
+        if a[mid] > k:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+def bs_recursive(a, low, high, k):
     if high >= low:
         mid = (low + high) // 2
         if a[mid] == k:
             return mid
         if a[mid] > k:
-            return binary_search(a, low, mid - 1, k)
-        return binary_search(a, mid + 1, high, k)
+            return bs_recursive(a, low, mid - 1, k)
+        return bs_recursive(a, mid + 1, high, k)
     return None
--- tests/clrs/arrays/test_binary_search.py
+++ tests/clrs/arrays/test_binary_search.py
@@ -1,10 +1,18 @@
 # Repository Library
-from clrs.arrays.binary_search import binary_search
+from clrs.arrays.binary_search import bs_iterative, bs_recursive
 
 
-def test_binary_search():
+def test_binary_search_iterative():
     a = [0, 1, 2, 3, 666]
-    assert a[binary_search(a, 0, len(a), 666)] == 666
-    assert a[binary_search(a, 0, len(a), 3)] == 3
-    assert a[binary_search(a, 0, len(a), 0)] == 0
-    assert binary_search(a, 0, len(a), -1) is None
+    assert a[bs_iterative(a, 0, len(a), 666)] == 666
+    assert a[bs_iterative(a, 0, len(a), 3)] == 3
+    assert a[bs_iterative(a, 0, len(a), 0)] == 0
+    assert bs_iterative(a, 0, len(a), -1) is None
+
+
+def test_binary_search_recursive():
+    a = [0, 1, 2, 3, 666]
+    assert a[bs_recursive(a, 0, len(a), 666)] == 666
+    assert a[bs_recursive(a, 0, len(a), 3)] == 3
+    assert a[bs_recursive(a, 0, len(a), 0)] == 0
+    assert bs_recursive(a, 0, len(a), -1) is None
