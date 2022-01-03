8cb7e415e8f95ae1702f87f90f7ab4339c6636c5
zzhao30 <zzhao30@ford.com>
2022-01-03 12:17:56 -0500

    CCs.

--- src/clrs/sorting_and_order_statistics/linear_sorting/radix_sort.py
+++ src/clrs/sorting_and_order_statistics/linear_sorting/radix_sort.py
@@ -1,16 +1,16 @@
 """
-Overview
-========
+8.3 Radix sort
+==============
 
 Radix sort sorts numbers based on least significant digits first. Only d passes through
-the array is required for an array whose numbers have d digits. If radix sort uses
-counting sort, then radix sort is also stable.
+the array is required for an array of n numbers where each number has d digits.
+
+Radix sort is stable if the counting_sort() procedure is used.
 
 Complexity
 ==========
 
-Stable
-O(d * (n + k))
+radix_sort(): O(d * (n + k)).
 """
 
 # Repository Library
@@ -19,8 +19,7 @@ from src.clrs.sorting_and_order_statistics.linear_sorting.counting_sort import (
 )
 
 
-def radix_sort(a, base=10):
-    exp = 1
+def radix_sort(a, base=10, exp=1):
     while max(a) / exp > 1:
         a = counting_sort(a, base, exp)
         exp *= base
