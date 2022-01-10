0e4d464350bc83325940823a29caeb036a9496ae
zzhao30 <zzhao30@ford.com>
2022-01-10 15:18:15 -0500

    CCs.

--- src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/knapsack.py
+++ src/clrs/advanced_design_and_analysis_techniques/dynamic_programming/knapsack.py
@@ -2,14 +2,17 @@
 Overview
 ========
 
-The objective of the knapsack problem is to maximize the total profit in the knapsack
-with a given weight limit. Given a list of items with corresponding profits and weights,
-which items should we include in the knapsack so that we maximize our profit without
-going over the given weight limit?
+The objective of the (0/1) knapsack problem is to maximize the total profit in the
+knapsack with a given weight limit. Given a list of items with corresponding profits and
+weights, which items should we include in the knapsack so that we maximize our profit
+without going over the given weight limit?
 
 Once we take an item, we gain profit but lose knapsack capacity. If we don't take an
 item, we keep the same profit but do not lose knapsack capacity. If taking an item puts
 up over the capacity limit, then the profit is the current profit without the item.
+
+Note that we cannot take fractional amounts of an item. We either take the whole item or
+none of it.
 
 Consider p = [1, 2, 5, 6], w = [2, 3, 4, 5], and c = 8. In this case, the answer is
 x = [0, 1, 0, 1] which corresponds to taking the second and fourth items only. We form
@@ -24,8 +27,8 @@ v   0   1   2   3   4   5   6   7   8 --> capacity
 
 items
 
-The first column is all 0's since a capacity of 0 means we can't anything and thus, all
-values are 0. The first row is all 0's since taking 0 items means the values are 0
+The first column is all 0's since a capacity of 0 means we can't take anything and thus,
+all values are 0. The first row is all 0's since taking 0 items means the values are 0
 regardless of the capacity.
 
 v[1][2] = 1 since we can take item 1 with weight 2 if we have a capacity of 2.
@@ -35,21 +38,31 @@ knapsack remains the same.
 For the rest of the entries, we check whether we maximize our value without taking the
 item i or else we take the item i and add its value to the total value of the knapsack.
 
-For example, the value of v[2][2] = max(v[1][2], v[1][2 - w[1]] = v[1][-1]) =
+For example, the value of v[2][2] = max(v[1][2], v[1][2 - w[2]] = v[1][-1] + p[2]) =
 max(v[1][2], -float("inf") + p[2]) = 1 since v[1][-1] does not exist. This means that we
 cannot take item 2 if our capacity is 2 since item 2's weight is 3 which exceeds the
 knapsack capacity. Thus, we look at v[1][2] which is the value if we take item 1
 assuming our capacity is 2.
 
-As another example, the value of v[2][3] = max(v[1][3], v[1][3 - w[1]] = v[1][0]) =
-max(v[1][3], v[1][0] + p[1]) = 2. This means that we can take item 2 if our capacity is
-3. By considering items 1 and 2 with a maximum capacity of 3, the highest value in our
-knapsack can be 2.
+As another example, the value of v[2][3] = max(v[1][3], v[1][3 - w[2]] = v[1][0] + p[2])
+= 2. This means that we can safely take item 2 if our capacity is 3. By considering
+items 1 and 2 with a maximum capacity of 3, the highest value in our knapsack can be 2,
+which corresponds to taking item 2 only.
 
-As a final example, the value of v[3][4] = max(v[2][4], v[2][4 - w[2]] = v[2][0] =
-max(v[2][4], v[2][0] + p[2]) = 5. This means that we can take item 3 if our capacity is
-4. By considering items 1, 2, and 3 with a maximum capacity of 4, the highest value in
-our knapsack can be 5.
+As a final example, the value of v[3][4] = max(v[2][4], v[2][4 - w[3]] = v[2][0] + p[3])
+= 5. This means that we can safely take item 3 if our capacity is 4. By considering
+items 1, 2, and 3 with a maximum capacity of 4, the highest value in our knapsack can be
+5, which corresponds to taking item 3 only.
+
+Intuition
+---------
+
+In order to maximize our total profit, we have to put items into our knapsack. We can
+put items into our knapsack as long as the weights of the items do not exceed the
+capacity of the knapsack. If we take an item, we have to ensure that its weight does not
+put us over the knapsack capacity. If it does not, then the total profit is increased by
+the value of the added object and our knapsack capacity is decreased appropriately for
+the next item to consider.
 
 Complexity
 ==========
