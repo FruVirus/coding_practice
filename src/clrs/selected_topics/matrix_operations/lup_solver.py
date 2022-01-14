3df3abe77361eb9c45dee4aec56e3589afc28ada
zzhao30 <zzhao30@ford.com>
2022-01-14 13:34:01 -0500

    CCs.

--- src/clrs/selected_topics/matrix_operations/lup_solver.py
+++ src/clrs/selected_topics/matrix_operations/lup_solver.py
@@ -1,47 +1,96 @@
 """
-Overview
-========
+28.1 Solving systems of linear equations
+========================================
 
 LUP Solver
 ----------
 
 We can formulate a linear system as a matrix equation in which each matrix or vector
-element belongs to a field, typically the real numbers (e.g., Ax = b). lup_solver()
-solves such a system of linear equations using a method called LUP decomposition. LUP
-decomposition is numerically stable and has the advantage of being faster in practice
-then the approach of computing the inverse of A and then multiplying by b.
+element belongs to a field, typically the real numbers.
 
-We only treat the case where there are exactly n equations in n unknowns; i.e., the rank
-of A is equal to the number n of unknowns and A is non-singular so that x = A^-1 * b
+We start with a set of linear equations in n unknowns x_1, x_2, ..., x_n:
+
+a_11 * x_1 + a_12 * x_2 + ... + a_1n * x_n = b_1,
+...
+a_n1 * x1 + a_n2 * x_ 2 + ... + a_nn * x_n = b_n.
+
+A solution to the equations is a set of values for x_1, x_2, ..., x_n that satisfy all
+of the equations simultaneously.
+
+If A is non-singular, it possesses an inverse A^(-1), and x = A^(-1) * b is the unique
+solution vector.
 
 If the rank of A is less than n, then the system is under-determined and the system will
-typically have infinitely many solutions, although no solutions at all if the equations
-are inconsistent.
+typically have infinitely many solutions, although it may have no solutions at all if
+the equations are inconsistent.
 
-If the rank of A exceeds the number n of unknowns, then the system is overdetermined and
-there may not exist any solutions.
+If the rank of A is greater than n, then the system is overdetermined and there may not
+exist any solutions.
+
+If we have exactly n equations in n unknowns, then we could compute A^(-1) and then
+solve for x using A^(-1) * b. This approach suffers in practice from numerical
+instability. LUP decomposition is numerically stable and has the further advantage of
+being faster in practice.
+
+Rank
+----
+
+The diagonal elements of the matrix returned by lu_decomp() can be used to determine the
+rank of the passed in matrix.
+
+Rank is the number of independent columns/rows of a matrix. If a column/row is a
+multiple of another column/row or is a combination or other columns/rows, then that
+column/row is not independent and does not contribute to the rank of a matrix.
+
+Rank can be found by performing Gaussian elimination on the input matrix and then
+counting the number of non-zero elements on the diagonal. After we perform Gaussian
+elimination, the resulting matrix is in row echelon form.
+
+A matrix is full row rank when each of the rows of the matrix are linearly independent
+and full column rank when each of the columns of the matrix are linearly independent.
+For a square matrix these two concepts are equivalent and we say the matrix is full rank
+if all rows and columns are linearly independent. A square matrix is full rank if and
+only if its determinant is nonzero.
+
+For a non-square matrix with m rows and n columns, it will always be the case that
+either the rows or columns (whichever is larger in number) are linearly dependent. Hence
+when we say that a non-square matrix is full rank, we mean that the row and column rank
+are as high as possible, given the shape of the matrix. So if there are more rows than
+columns (m > n), then the matrix is full rank if the matrix is full column rank.
+
+Overview of LUP decomposition
+-----------------------------
 
 The idea of LUP decomposition is to find three n x n matrices L, U, and P such that:
 
-    PA = LU
+    PA = LU (28.4)
 
 where:
     L is a unit lower-triangular matrix
     U is an upper-triangular matrix
     P is a permutation matrix
 
-Every non-singular matrix posses such a decomposition.
+We call matrices L, U, and P satisfying equation (28.4) an LUP decomposition of the
+matrix A. Every non-singular matrix posses such a decomposition.
 
-Once we have found an LUP decomposition for A, we can solve the system of equations,
-Ax = b, by solving only the triangular linear systems by multiplying both sides of
-Ax = b by, which yields PAx = Pb. This amounts to permuting the system of equations.
+Computing an LUP decomposition for the matrix A has the advantage that we can more
+easily solve linear systems when they are triangular, as is the case for both matrices L
+and U. Once we have found an LUP decomposition for A, we can solve the system of
+equations, Ax = b, by solving only the triangular linear systems, as follows.
+Multiplying both sides of Ax = b by, which yields the equivalent equation PAx = Pb,
+which amounts to permuting the system of equations. Using our decomposition (28.4), we
+obtain:
 
     PAx = Pb
-    LUx = Pb --> y = Ux
-        Ly = Pb --> solve for y using forward substitution
-        Ux = y --> solve for x using back substitution
+    LUx = Pb --> define y = Ux, where x is the desired solution vector
 
-    since P is invertible, P^(-1)PA = P^(-1)LU --> A = P^(-1)LU
+    Ly = Pb --> solve for y using forward substitution
+    Ux = y --> solve for x using back substitution
+
+Because the permutation matrix P is invertible, multiplying both sides of equation
+(28.4) by P^(-1) gives P^(-1)PA = P^(-1)LU so that A = P^(-1)LU
+
+Hence, the vector x is our solution to Ax = b:
 
     Ax = P^(-1)LUx
        = P^(-1)Ly
@@ -93,33 +142,6 @@ A = (1      0)      (a_11    w.T)
     (v/a_11 L') (0, U')
   = LU
 
-
-Rank
-----
-
-The diagonal elements of the matrix returned by lu_decomp() can be used to determine the
-rank of the passed in matrix.
-
-Rank is the number of independent columns/rows of a matrix. If a column/row is a
-multiple of another column/row or is a combination or other columns/rows, then that
-column/row is not independent and does not contribute to the rank of a matrix.
-
-Rank can be found by performing Gaussian elimination on the input matrix and then
-counting the number of non-zero elements on the diagonal. After we perform Gaussian
-elimination, the resulting matrix is in row echelon form.
-
-A matrix is full row rank when each of the rows of the matrix are linearly independent
-and full column rank when each of the columns of the matrix are linearly independent.
-For a square matrix these two concepts are equivalent and we say the matrix is full rank
-if all rows and columns are linearly independent. A square matrix is full rank if and
-only if its determinant is nonzero.
-
-For a non-square matrix with m rows and n columns, it will always be the case that
-either the rows or columns (whichever is larger in number) are linearly dependent. Hence
-when we say that a non-square matrix is full rank, we mean that the row and column rank
-are as high as possible, given the shape of the matrix. So if there are more rows than
-columns (m > n), then the matrix is full rank if the matrix is full column rank.
-
 LUP Decomposition
 -----------------
 
@@ -155,11 +177,12 @@ PA = (1   0)QA
 Complexity
 ==========
 
-Theta(n^3) for LU decomposition
-Theta(n^3) for LUP decomposition
-Theta(n^2) for forward substitution
-Theta(n^2) for back substitution
-Theta(n^2) for solving the system
+Time
+----
+
+lu_decomp(), lup_decomp(): Theta(n^3).
+lup_solver(): Theta(n^2) for forward substitution, Theta(n^2) for backward substitution,
+Theta(n^2) for solving the system of equations.
 """
 
 
@@ -188,10 +211,9 @@ def lup_solver(a, b, p=None, decomp=True, lup=True):
     if decomp:
         p = lu_decomp(a) if lup is False else lup_decomp(a)
     n = len(a)
-    x, y = [0] * n, [0] * n
+    b, x, y = [b[i] if p is None else b[p[i]] for i in range(n)], [0] * n, [0] * n
     for i in range(n):
-        b_ = b[i] if p is None else b[p[i]]
-        y[i] = b_ - sum(a[i][j] * y[j] for j in range(i))
+        y[i] = b[i] - sum(a[i][j] * y[j] for j in range(i))
     for i in reversed(range(n)):
         x[i] = (y[i] - sum(a[i][j] * x[j] for j in range(i + 1, n))) / a[i][i]
     return x
