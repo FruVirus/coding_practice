80b9593cfe81c0c3a8808e7086ad6d3717e84431
zzhao30 <zzhao30@ford.com>
2022-01-16 16:21:27 -0500

    CCs.

--- src/clrs/selected_topics/linear_programming/simplex.py
+++ src/clrs/selected_topics/linear_programming/simplex.py
@@ -115,7 +115,122 @@ integral.
 Pivoting
 --------
 
-XXX
+The procedure pivot() takes as input a slack form, given by the tuple
+(N, B, A, b, c, v), the index l of the leaving variable x_l, and the index e of the
+entering variable x_e. It returns the tuple (N_hat, Bhat, Ahat, bhat, chat, vhat)
+describing the new slack form.
+
+The entries of A and Ahat in pivot() are actually the negatives of the coefficients that
+appear in the slack form (or the same as the coefficients that appear in the standard
+form).
+
+The procedure pivot() works in three steps:
+
+1. It computes the coefficients of the equation for the new basic variable x_e.
+2. It updates the coefficients of the remaining constraints.
+3. It updates the coefficients of the objective function.
+
+pivot() is only ever called when A[l][e] is not 0.
+
+The formal simplex algorithm
+----------------------------
+
+We assume that we have a procedure initialize_simplex(A, b, c) that takes as input a
+linear program in standard form, that is an m x n matrix A, an m-vector b, and an
+n-vector c. If the problem is infeasible, the procedure returns a message that the
+problem is infeasible and then terminates. Otherwise, the procedures returns a slack
+form for which the initial basic solution is feasible.
+
+The procedure simplex() takes as input a linear program in standard form, as just
+described. It returns an n-vector x_bar that is an optimal solution to the linear
+program.
+
+The procedure simplex() works as follows:
+
+1. It calls the procedure initialize_simplex(A, b, c), described above, which either
+determines that the linear program is infeasible or returns a slack form for which the
+basic solution is feasible.
+2. The while-loop forms the main part of the algorithm. If all coefficients in the
+objective function are negative, then the while-loop terminates. Otherwise, we select a
+variable x_e, whose coefficient in the objective function is positive, as the entering
+variable.
+3. Next, we check each constraint and pick the one that most severely limits the amount
+by which we can increase x_e without violating any of the non-negativity constraints;
+the basic variable associated with this constraint ix x_l. If none of the constraints
+limits the amount by which the entering variable can increase, the algorithm returns
+"Unbounded!". Otherwise, we exchange the roles of the entering and leaving variables
+by calling the procedure pivot().
+4. Finally, we compute a solution, x_bar, for the original linear-programming variables
+by setting all the nonbasic variables to 0 and each basic variable to b.
+
+Termination
+-----------
+
+No iteration of the procedure simplex() can decrease the objective value associated with
+the basic solution. Unfortunately, it is possible that an iteration leaves the objective
+value unchanged. This phenomenon is called degeneracy.
+
+Degeneracy can prevent the simplex algorithm from terminating, because it can lead to a
+phenomenon known as cycling: the slack forms at two different iterations of the
+procedure simplex() are identical. Because of degeneracy, simplex() could choose a
+sequence of pivot operations that leave the objective value unchanged but repeat a slack
+form within the sequence. Since simplex() is a deterministic algorithm, if it cycles,
+then it will cycle through the same series of slack forms forever, never terminating.
+
+Cycling is the only reason that simplex() might not terminate.
+
+Cycling is theoretically possible, but extremely rare. We can prevent it by choosing the
+entering and leaving variables somewhat more carefully. One option is to perturb the
+input slightly so that it is impossible to have two solutions with the same objective
+value. Another option is to break ties by always choosing the variable with the smallest
+index, a strategy known as Bland's rule.
+
+29.4 Duality
+============
+
+Linear-programming duality enables us to prove that a solution is indeed optimal. Given
+a linear program in which the objective is to maximize, we can formulate a dual linear
+program in which the objective is to minimize and whose optimal value is identical to
+that of the original (primal) linear program.
+
+To form the dual, we change the maximization to a minimization, exchange the roles of
+coefficients on the RHS and the objective function, and replace each <= by a >=. Each of
+the m constraints in the primal has an associated variable y_i in the dual, and each of
+the n constraints in the dual has an associated variable x_j in the primal.
+
+The optimal value of the dual linear program is always equal to the optimal value of the
+primal linear program. Furthermore, the simplex algorithm actually implicitly solves
+both the primal and dual programs simultaneously, thereby providing a proof of
+optimality.
+
+The negatives of the coefficients of the primal objective function are the values of the
+dual variables.
+
+29.5 The initial basic feasible solution
+========================================
+
+Finding an initial solution
+---------------------------
+
+A linear program can be feasible, yet the initial basic solution might not be feasible.
+Thus, the procedure initialize_simplex() cannot just return any slack form. In order to
+determine whether a linear program has any feasible solutions, we will formulate an
+auxiliary linear program. For this auxiliary linear program we can find (with a little
+work) a slack form for which the basic solution is feasible. Furthermore, the solution
+of this auxiliary linear program determines whether the initial linear program is
+feasible and if so, it provides a feasible solution with which we can initialize
+simplex().
+
+In order to determine whether a linear program has any feasible solutions, we formulate
+an auxiliary linear program, L', using the procedure initialize_aux(). For L', we can
+find a slack form for which the basic solution is feasible. Furthermore, the solution of
+L' determines whether the original linear program, L, is feasible and if so, it provides
+a feasible solution with which we can initialize simplex().
+
+L is feasible iff the optimal objective value of L' is 0. If the optimal objective value
+of L' is negative, then L does not have a feasible solution.
+
+The input to initialize_simplex() is a linear program L in standard form.
 
 Fundamental Theorem of Linear Programming
 -----------------------------------------
@@ -129,87 +244,6 @@ Any linear program, L, given in standard form, either:
 If L is infeasible, the simplex algorithm returns "Infeasible!". If L is unbounded, the
 simplex algorithm returns "Unbounded!". Otherwise, the simplex algorithm returns an
 optimal solution with a finite objective value.
-
-initialize_simplex()
---------------------
-
-A linear program can be feasible, yet the initial basic solution might not be feasible
-(e.g., the initial basic solution might set the solution to be negative and violate the
-non-negativity constraints). In order to determine whether a linear program has any
-feasible solutions, we formulate an auxility linear program, L'. For L', we can find a
-slack form for which the basic solution is feasible. Furthermore, the solution of L'
-determines whether L is feasible and if so, it provides a feasible solution with which
-we can initialize simplex().
-
-L is feasible iff the optimal objective value of L' is 0. If the optimal objective value
-of L' is negative, then L does not have a feasible solution.
-
-The input to initialize_simple() is a linear program L in standard form.
-
-pivot()
--------
-
-A pivot operation chooses a nonbasic variable x_e, called the entering variable, and a
-basic variable x_l, called the leaving variable, and exchanges their roles in the linear
-program.
-
-The entries of A and Ahat in pivot() are actually the negatives of the coefficients that
-appear in the slack form (or the same as the coefficients that appear in the standard
-form).
-
-pivot() is only ever called when A[l][e] is not 0.
-
-simplex()
----------
-
-simplex() takes as input a linear program in standard form and return an n-vector,
-x_bar, that is an optimal solution to the linear program. Each iteration of the
-while-loop in simplex() exchanges the role of a basic and nonbasic variable by calling
-pivot(). The slack form is equivalent to the one from the previous iteration which, by
-the loop invariant, is equivalent to the initial slack form.
-
-Degeneracy and Cycling
-----------------------
-
-No iteration of simplex() can decrease the objective value associated with the basic
-solution. However, it is possible that an iteration leaves the objective value
-unchanged. This phenomenon is called degeneracy.
-
-Degeneracy can prevent the simplex algorithm from terminating, because it can lead to a
-phenomenon known as cycling: the slack forms at two different iterations of simplex()
-are identical. Because of degeneracy, the simplex algorithm could choose a sequence of
-pivot operations that leave the objective value unchanged but repeat a slack form within
-the sequence. Since simplex() is a deterministic algorithm, if it cycles, then it will
-cycle through the same series of slack forms forever, never terminating.
-
-Cycling is the only reason that simplex() might not terminate. if simplex() fails to
-terminate in at most (n + m) choose m iterations, then it cycles. There are n + m
-variables and |B| = m; therefore, there are at most (n + m) choose m ways to choose B.
-
-We can prevent cycling by perturbing the input slightly so that it is impossible to have
-two solutions with the same object value. Another option is to break ties by always
-choosing the variable with the smallest index, a strategy known as Bland's rule.
-
-Duality
--------
-
-Linear-programming duality enables us to prove that a solution is indeed optimal. Given
-a linear program in which the objective is to maximize, we can formulate a dual linear
-program in which the objective is to minimize and whose optimal vlaue is identical to
-that of the original (primal) linear program.
-
-To form the dual, we change the maximization to a minimization, exhcnage the roles of
-coefficients on the RHS and the objective function, and replace each <= by a >=. Each of
-the m constraints in the primal has an associated variable y_i in the dual, and each of
-the n constraints in the dual has an associated variable x_j in the primal.
-
-The optimal value of the dual linear program is always equal to the optimal value of the
-primal linear program. Furthermore, the simplex algorithm actually implicitly solves
-both the primal and dual programs simultaneously, thereby providing a proof of
-optimality.
-
-The negatives of the coefficients of the primal objective function are the values of the
-dual variables.
 
 Complexity
 ==========
@@ -245,9 +279,7 @@ def initialize_aux(A, k):
 
 
 def initialize_simplex(A, b, c):
-    n, m = len(A[0]), len(A)
-    assert m == len(b) and n == len(c)
-    k = b.index(min(b))
+    n, m, k = len(A[0]), len(A), b.index(min(b))
     if b[k] >= 0:
         return list(range(n)), list(range(n, n + m)), A, b, c, 0
     N, Naux, Baux, Aaux, caux, l = initialize_aux(A, k)
@@ -270,30 +302,6 @@ def initialize_simplex(A, b, c):
 
 def pivot(N, B, A, b, c, e, l, v):
     assert e in N and l in B
-    Nhat, Bhat, Ahat, bhat = update_constraints(N, B, A, b, e, l)
-    chat, vhat = update_objective(N, c, e, l, v, Nhat, Bhat, Ahat, bhat)
-    return Nhat, Bhat, Ahat, bhat, chat, vhat
-
-
-def return_aux(N, c, Naux, Baux, Aaux, baux, vaux):
-    n, m = len(Aaux[0]), len(Aaux)
-    Naux.pop(0)
-    Ahat = [[0] * len(Naux) for _ in range(len(Baux))]
-    for row in range(m):
-        for col in range(1, n):
-            Ahat[row][col - 1] = Aaux[row][col]
-    vaux += sum(-baux[i] for i, _ in enumerate(include(N, Baux)))
-    caux = [0] * len(Naux)
-    for i, _ in enumerate(include(N, Naux)):
-        caux[i] = c[i]
-    for i in include(N, Baux):
-        cval = -c[N.index(i)]
-        for j, x in enumerate(Ahat[Baux.index(i)]):
-            caux[j] += x * cval
-    return Naux, Baux, Ahat, baux, caux, vaux
-
-
-def update_constraints(N, B, A, b, e, l):
     n, m, Ne, Bl = len(N), len(B), N.index(e), B.index(l)
     Ahat, bhat = [[0] * n for _ in range(m)], [0] * m
     Nhat, Bhat = list(N), list(B)
@@ -312,18 +320,30 @@ def update_constraints(N, B, A, b, e, l):
             Nhatj = Nhat.index(j)
             Ahat[Bhati][Nhatj] = A[Bi][N.index(j)] - pivot_var * Ahat[Bhate][Nhatj]
         Ahat[Bhati][Nhatl] = -pivot_var * Ahat[Bhate][Nhatl]
-    return Nhat, Bhat, Ahat, bhat
-
-
-def update_objective(N, c, e, l, v, Nhat, Bhat, Ahat, bhat):
-    Ne, Nhatl, Bhate = N.index(e), Nhat.index(l), Bhat.index(e)
-    v += c[Ne] * bhat[Bhate]
-    chat = [0] * len(Nhat)
-    for i in exclude(N, e):
+    chat, vhat = [0] * len(Nhat), v + c[Ne] * bhat[Bhate]
+    for i in no_e:
         Nhati = Nhat.index(i)
         chat[Nhati] = c[N.index(i)] - c[Ne] * Ahat[Bhate][Nhati]
     chat[Nhatl] = -c[Ne] * Ahat[Bhate][Nhatl]
-    return chat, v
+    return Nhat, Bhat, Ahat, bhat, chat, vhat
+
+
+def return_aux(N, c, Naux, Baux, Aaux, baux, vaux):
+    n, m = len(Aaux[0]), len(Aaux)
+    Naux.pop(0)
+    Ahat = [[0] * len(Naux) for _ in range(len(Baux))]
+    for row in range(m):
+        for col in range(1, n):
+            Ahat[row][col - 1] = Aaux[row][col]
+    vaux += sum(-baux[Baux.index(i)] for i in include(N, Baux))
+    caux = [0] * len(Naux)
+    for i in include(N, Naux):
+        caux[Naux.index(i)] = c[N.index(i)]
+    for i in include(N, Baux):
+        cval = -c[N.index(i)]
+        for j, x in enumerate(Ahat[Baux.index(i)]):
+            caux[j] += x * cval
+    return Naux, Baux, Ahat, baux, caux, vaux
 
 
 def simplex(A, b, c, N=None, B=None, v=None):
@@ -332,17 +352,18 @@ def simplex(A, b, c, N=None, B=None, v=None):
         if isinstance(val, str):
             return val
         N, B, A, b, c, v = val
-    delta = [0] * len(B)
-    while any(c[i] > 1e-12 for i in range(len(N))):
-        e = min([x for i, x in enumerate(N) if c[i] > 0])
+    delta = [0] * len(A)
+    while any(c[N.index(i)] > 1e-12 for i in N):
+        e = min([i for i in N if c[N.index(i)] > 0])
         Ne = N.index(e)
-        for Bi, _ in enumerate(B):
+        for i in B:
+            Bi = B.index(i)
             delta[Bi] = b[Bi] / A[Bi][Ne] if A[Bi][Ne] > 0 else float("inf")
         l = B[delta.index(min(delta))]
         if delta[B.index(l)] == float("inf"):
             return "Unbounded!"
         N, B, A, b, c, v = pivot(N, B, A, b, c, e, l, v)
     x_bar = [0] * (max(N[-1], B[-1]) + 1)
-    for index, i in enumerate(B):
-        x_bar[i] = b[index]
+    for i in B:
+        x_bar[i] = b[B.index(i)]
     return x_bar, N, B, A, b, c, v
