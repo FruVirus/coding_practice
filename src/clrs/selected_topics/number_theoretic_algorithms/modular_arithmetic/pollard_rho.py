d17538503b5d1d594bf71e8e22a1fc952528230f
zzhao30 <zzhao30@ford.com>
2022-01-20 14:11:41 -0500

    CCs.

--- src/clrs/selected_topics/number_theoretic_algorithms/modular_arithmetic/pollard_rho.py
+++ src/clrs/selected_topics/number_theoretic_algorithms/modular_arithmetic/pollard_rho.py
@@ -1,10 +1,18 @@
 """
-Overview
-========
+31.9 Integer factorization
+==========================
 
-We want to factor an integer n into a product of primes. Trial division by all integers
-up to R is guaranteed to factor completely any number up to R^2. For the same amount of
-work, the Pollard's rho heuristic factors any number up to R^4 (unless we are unlucky).
+Suppose w have an integer n that we wish to factor, that is, to decompose into a product
+of primes. The Miller Rabin primality test may tell us that n is composite, but it does
+not tell us the prime factors of n. Factoring a large integer n seems to be much more
+difficult than simpy determining whether n is prime of composite.
+
+Pollard's rho heuristic
+-----------------------
+
+Trial division by all integers up to R is guaranteed to factor completely any number up
+to R^2. For the same amount of work, the Pollard's rho heuristic factors any number up
+to R^4 (unless we are unlucky).
 
 Since the procedure is only a heuristic, neither its running time or its success is
 guaranteed, although the procedure is highly effective in practice. Another advantage of
@@ -16,12 +24,16 @@ with no guarantee that it will print any divisors. However, we have good reason 
 expect that the heuristic will print a factor p of n after Theta(sqrt(p)) iterations of
 the while loop.
 
-Eventually, pollard_rho will start to repeat values---it can then exit at this point.
+Eventually, Pollard's rho heuristic will start to repeat values---it can then exit at
+this point.
 
 Complexity
 ==========
 
-Theta(sqrt(p)) arithmetic operations, where p is a factor of n
+Time
+----
+
+pollard_rho(): Theta(sqrt(p)) arithmetic operations, where p is a factor of n.
 """
 
 # Standard Library
