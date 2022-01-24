7ad96c39f501b3cd12d7bd68c2348f4052b1e865
zzhao30 <zzhao30@ford.com>
2022-01-24 11:01:54 -0500

    CCs.

--- src/clrs/data_structures/elementary_data_structures/heap_queue.py
+++ src/clrs/data_structures/elementary_data_structures/heap_queue.py
@@ -6,7 +6,7 @@ The (binary) heap data structure is an array object that we can view as a nearly
 complete binary tree. Each node of the tree corresponds to an element of the array. The
 tree is completely filled on all levels except possibly the lowest, which is filled from
 the left up to a point. An array A that represents a heap is an object with two
-attributes: A.length, which gives hte number of elements in the array, and A.heap-size,
+attributes: A.length, which gives the number of elements in the array, and A.heap-size,
 which represents how many elements in the heap are stored within array A. That is,
 although A[1...A.length] may contain numbers, only the elements A[1...A.heap-size],
 where 0 <= A.heap-size <= A.length, are valid elements of the heap. The root of the tree
