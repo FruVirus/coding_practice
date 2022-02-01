cb7ec7f62b61414c8de12c77e321028a223763b4
zzhao30 <zzhao30@ford.com>
2022-02-01 17:15:32 -0500

    CCs.

--- src/clrs/data_structures/elementary_data_structures/max_subarray.py
+++ src/clrs/data_structures/elementary_data_structures/max_subarray.py
@@ -84,7 +84,10 @@ array, a.
 
 In other words, the value for clow resets to i each time csum becomes negative, which
 means that the current low index is always the index that gave the previous negative
-change in the cumulative sum.
+change in the cumulative sum. Once csum goes negative, we don't have to worry about the
+values in a that came before (old) clow since the cumulative sum of those values became
+negative. Thus, we can reset clow to i and start the cumulative sum over again with
+a_[i].
 
 Complexity
 ==========
