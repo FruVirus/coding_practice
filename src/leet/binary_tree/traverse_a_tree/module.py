45b5b6e9b75413aad9373ae9a4d554c79dc7709b
zzhao30 <zzhao30@ford.com>
2022-02-25 11:11:06 -0500

    CCs.

--- src/leet/binary_tree/traverse_a_tree/module.py
+++ src/leet/binary_tree/traverse_a_tree/module.py
@@ -38,13 +38,6 @@ All: O(n).
 from collections import deque
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 class Solution:
     def __init__(self):
         self.order = []
