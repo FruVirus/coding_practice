5d8609529727a08b879a7b5292ca1209ba8425ae
zzhao30 <zzhao30@ford.com>
2022-02-25 17:02:47 -0500

    CCs.

--- src/leet/queue_and_stack/conclusion/module.py
+++ src/leet/queue_and_stack/conclusion/module.py
@@ -43,12 +43,8 @@ class MyQueue:
     def __init__(self):
         self.s1, self.s2 = [], []
 
-    def push(self, x):
-        self.s1.append(x)
-
-    def pop(self):
-        self.peek()
-        return self.s2.pop()
+    def empty(self):
+        return not (self.s1 or self.s2)
 
     def peek(self):
         if not self.s2:
@@ -56,8 +52,12 @@ class MyQueue:
                 self.s2.append(self.s1.pop())
         return self.s2[-1]
 
-    def empty(self):
-        return not (self.s1 or self.s2)
+    def pop(self):
+        self.peek()
+        return self.s2.pop()
+
+    def push(self, x):
+        self.s1.append(x)
 
 
 class MyStack:
