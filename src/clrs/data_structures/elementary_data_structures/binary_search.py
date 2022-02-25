6a19f80c4be648c0497e9a9005f435ec2d7b3ed1
zzhao30 <zzhao30@ford.com>
2022-02-25 16:26:35 -0500

    CCs.

--- src/clrs/data_structures/elementary_data_structures/binary_search.py
+++ src/clrs/data_structures/elementary_data_structures/binary_search.py
@@ -5,17 +5,42 @@ Overview
 Binary search searches for the index of an item k in an array by recursively splitting
 the array in two. The input array is assumed to be sorted in increasing order.
 
+Intuition
+---------
+
+bs_iterative_one() is used to search for an element or condition which can be determined
+by accessing a single index in the array.
+
+bs_iterative_two() is used to search for an element or condition which requires
+accessing the current index and its immediate right neighbor's index in the array.
+Post-processing is required since the loop/recursion ends when you have 1 element left.
+Thus, we need to assess if the remaining element meets the condition. This guarantees
+that the search space is at least 2 in size at each step.
+
+bs_iterative_three() is used to search for an element or condition which requires
+accessing the current index and its immediate left and right neighbor's index in the
+array. Post-processing is required since the loop/recursion ends when you have 2
+elements left. Thus, we need to assess if the remaining elements meet the condition.
+This guarantees that th search space is at least 3 in size at each step.
+
 Complexity
 ==========
 
 Time
 ----
 
-bs_iterative() and bs_recursive(): O(lg n).
+bs_iterative_one(), bs_iterative_two(), bs_iterative_three(), and bs_recursive():
+O(lg n).
+
+Space
+-----
+
+bs_recursive(): O(lg n) stack space.
 """
 
 
-def bs_iterative(a, low, high, k):
+def bs_iterative_one(a, k):
+    low, high = 0, len(a) - 1
     while low <= high:
         mid = low + ((high - low) // 2)
         if a[mid] == k:
@@ -24,6 +49,36 @@ def bs_iterative(a, low, high, k):
             high = mid - 1
         else:
             low = mid + 1
+    return None
+
+
+def bs_iterative_two(a, k):
+    low, high = 0, len(a)
+    while low < high:
+        mid = low + ((high - low) // 2)
+        if a[mid] == k:
+            return mid
+        if a[mid] > k:
+            high = mid
+        else:
+            low = mid + 1
+    return low if low != len(a) and a[low] == k else None
+
+
+def bs_iterative_three(a, k):
+    low, high = 0, len(a) - 1
+    while low + 1 < high:
+        mid = low + ((high - low) // 2)
+        if a[mid] == k:
+            return mid
+        if a[mid] > k:
+            high = mid
+        else:
+            low = mid
+    if a[low] == k:
+        return low
+    if a[high] == k:
+        return high
     return None
 
 
--- tests/clrs/data_structures/elementary_data_structures/test_binary_search.py
+++ tests/clrs/data_structures/elementary_data_structures/test_binary_search.py
@@ -1,21 +1,39 @@
 # Repository Library
 from clrs.data_structures.elementary_data_structures.binary_search import (
-    bs_iterative,
+    bs_iterative_one,
+    bs_iterative_three,
+    bs_iterative_two,
     bs_recursive,
 )
 
 
-def test_binary_search_iterative():
+def test_binary_search_iterative_one():
     a = [0, 1, 2, 3, 666]
-    assert a[bs_iterative(a, 0, len(a), 666)] == 666
-    assert a[bs_iterative(a, 0, len(a), 3)] == 3
-    assert a[bs_iterative(a, 0, len(a), 0)] == 0
-    assert bs_iterative(a, 0, len(a), -1) is None
+    assert a[bs_iterative_one(a, 666)] == 666
+    assert a[bs_iterative_one(a, 3)] == 3
+    assert a[bs_iterative_one(a, 0)] == 0
+    assert bs_iterative_one(a, -1) is None
+
+
+def test_binary_search_iterative_two():
+    a = [0, 1, 2, 3, 666]
+    assert a[bs_iterative_two(a, 666)] == 666
+    assert a[bs_iterative_two(a, 3)] == 3
+    assert a[bs_iterative_two(a, 0)] == 0
+    assert bs_iterative_two(a, -1) is None
+
+
+def test_binary_search_iterative_three():
+    a = [0, 1, 2, 3, 666]
+    assert a[bs_iterative_three(a, 666)] == 666
+    assert a[bs_iterative_three(a, 3)] == 3
+    assert a[bs_iterative_three(a, 0)] == 0
+    assert bs_iterative_three(a, -1) is None
 
 
 def test_binary_search_recursive():
     a = [0, 1, 2, 3, 666]
-    assert a[bs_recursive(a, 0, len(a), 666)] == 666
-    assert a[bs_recursive(a, 0, len(a), 3)] == 3
-    assert a[bs_recursive(a, 0, len(a), 0)] == 0
-    assert bs_recursive(a, 0, len(a), -1) is None
+    assert a[bs_recursive(a, 0, len(a) - 1, 666)] == 666
+    assert a[bs_recursive(a, 0, len(a) - 1, 3)] == 3
+    assert a[bs_recursive(a, 0, len(a) - 1, 0)] == 0
+    assert bs_recursive(a, 0, len(a) - 1, -1) is None
