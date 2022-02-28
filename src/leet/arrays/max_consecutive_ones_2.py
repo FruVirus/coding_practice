902e8a3d85637d5358bf0f71a3b08e4be252a9ca
zzhao30 <zzhao30@ford.com>
2022-02-28 13:39:06 -0500

    CCs.

---
+++ src/leet/arrays/max_consecutive_ones_2.py
@@ -0,0 +1,31 @@
+"""
+Max Consecutive Ones II
+-----------------------
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array if
+you can flip at most one 0.
+
+Complexity
+==========
+
+Time
+----
+
+find_max_consecutive_ones(): O(n).
+
+Space
+-----
+
+find_max_consecutive_ones(): O(1).
+"""
+
+
+def find_max_consecutive_ones(nums):
+    last_zero, longest_ones, num_ones = -1, 0, 0
+    for i in range(len(nums)):
+        if nums[i] == 0:
+            num_ones, last_zero = i - last_zero, i
+        else:
+            num_ones += 1
+        longest_ones = max(longest_ones, num_ones)
+    return longest_ones
