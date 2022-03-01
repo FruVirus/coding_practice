2869cbd58cb6c9e4731963598d0d4a55a779f67e
FruVirus <zhen.zhao6@gmail.com>
2022-02-28 20:19:16 -0500

    CCs.

--- src/leet/arrays/merge_sorted_array.py
+++ src/leet/arrays/merge_sorted_array.py
@@ -29,13 +29,12 @@ merge(): O(1).
 
 
 def merge(nums1, m, nums2, n):
-    i, j = m - 1, n - 1
-    for k in reversed(range(n + m)):
-        if j < 0:
-            break
+    i, j, k = m - 1, n - 1, m + n - 1
+    while j >= 0:
         if i >= 0 and nums1[i] > nums2[j]:
             nums1[k] = nums1[i]
             i -= 1
         else:
             nums1[k] = nums2[j]
             j -= 1
+        k -= 1
