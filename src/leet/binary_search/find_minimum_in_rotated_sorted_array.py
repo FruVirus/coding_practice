8c45a62da9001df5eb88163b8978d2bb58748644
zzhao30 <zzhao30@ford.com>
2022-03-01 16:37:07 -0500

    CCs.

--- src/leet/binary_search/find_minimum_in_rotated_sorted_array.py
+++ src/leet/binary_search/find_minimum_in_rotated_sorted_array.py
@@ -1,20 +1,20 @@
 """
-Find Minimum in Rotated Sorted Array
-------------------------------------
+Find Minimum in Rotated Sorted Array II
+---------------------------------------
 
 Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
-For example, the array nums = [0,1,2,4,5,6,7] might become:
+For example, the array nums = [0,1,4,4,5,6,7] might become:
 
-    [4,5,6,7,0,1,2] if it was rotated 4 times.
-    [0,1,2,4,5,6,7] if it was rotated 7 times.
+    [4,5,6,7,0,1,4] if it was rotated 4 times.
+    [0,1,4,4,5,6,7] if it was rotated 7 times.
 
 Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the
 array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
 
-Given the sorted rotated array nums of unique elements, return the minimum element of
-this array.
+Given the sorted rotated array nums that may contain duplicates, return the minimum
+element of this array.
 
-You must write an algorithm that runs in O(log n) time.
+You must decrease the overall operation steps as much as possible.
 
 Complexity
 ==========
@@ -22,7 +22,7 @@ Complexity
 Time
 ----
 
-find_min(): O(lg n).
+find_min(): O(lg n)/O(n) expected/worse case.
 
 Space
 -----
@@ -35,12 +35,11 @@ def find_min(nums):
     low, high = 0, len(nums) - 1
     while low < high:
         mid = low + (high - low) // 2
-        if nums[mid] > nums[mid + 1]:
-            return nums[mid + 1]
-        if nums[mid] < nums[mid - 1]:
-            return nums[mid]
-        if nums[mid] > nums[0]:
-            low = mid + 1
+        if nums[low] == nums[mid] == nums[high]:
+            low += 1
+            high -= 1
+        elif nums[mid] <= nums[high]:
+            high = mid
         else:
-            high = mid - 1
-    return nums[0]
+            low = mid + 1
+    return nums[low]
