bf7df00f35952915615782618a878cb6d7a52c64
zzhao30 <zzhao30@ford.com>
2022-03-01 12:01:21 -0500

    CCs.

--- src/leet/linked_list/linked_list_cycle.py
+++ src/leet/linked_list/linked_list_cycle.py
@@ -1,14 +1,4 @@
 """
-Linked List Cycle
------------------
-
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
-
-There is a cycle in a linked list if there is some node in the list that can be reached
-again by continuously following the next pointer.
-
-Return true if there is a cycle in the linked list. Otherwise, return false.
-
 Linked List Cycle II
 --------------------
 
@@ -26,12 +16,12 @@ Complexity
 Time
 ----
 
-detect_cycle() and has_cycle(): O(n).
+detect_cycle(): O(n).
 
 Space
 -----
 
-detect_cycle() and has_cycle(): O(1).
+detect_cycle(): O(1).
 """
 
 
@@ -49,14 +39,3 @@ def detect_cycle(head):
     while ptr1 != ptr2:
         ptr1, ptr2 = ptr1.next, ptr2.next
     return ptr1
-
-
-def has_cycle(head):
-    if not head:
-        return False
-    slow, fast = head, head.next
-    while fast and fast.next:
-        slow, fast = slow.next, fast.next.next
-        if slow is fast:
-            return True
-    return False
