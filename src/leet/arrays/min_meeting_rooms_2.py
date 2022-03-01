5a448e47c91e1226e6da5fcee1f2ff2fc3447c7f
FruVirus <zhen.zhao6@gmail.com>
2022-02-28 21:14:02 -0500

    CCs.

--- src/leet/arrays/min_meeting_rooms_2.py
+++ src/leet/arrays/min_meeting_rooms_2.py
@@ -23,11 +23,11 @@ min_meeting_rooms(): O(n).
 def min_meeting_rooms(intervals):
     start_times = sorted([i[0] for i in intervals])
     end_times = sorted([i[1] for i in intervals])
-    start_ptr = end_ptr = num_rooms = 0
-    while start_ptr < len(intervals) and end_ptr < len(intervals):
-        if start_times[start_ptr] < end_times[end_ptr]:
+    start = end = num_rooms = 0
+    while start < len(intervals) and end < len(intervals):
+        if start_times[start] < end_times[end]:
             num_rooms += 1
         else:
-            end_ptr += 1
-        start_ptr += 1
+            end += 1
+        start += 1
     return num_rooms
