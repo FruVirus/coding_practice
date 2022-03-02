079940e0cd1d9f2342921f71691f540d429d2778
zzhao30 <zzhao30@ford.com>
2022-03-02 11:44:31 -0500

    CCs.

---
+++ src/leet/binary_tree/symmetric_tree.py
@@ -0,0 +1,34 @@
+"""
+Symmetric Tree
+--------------
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric
+around its center).
+
+Complexity
+==========
+
+Time
+----
+
+is_symmetric: O(n).
+
+Space
+-----
+
+is_symmetric: O(n).
+"""
+
+
+def is_symmetric(root):
+    stack = [(root.left, root.right)]
+    while stack:
+        right, left = stack.pop()
+        if left and right:
+            if left.val != right.val:
+                return False
+            stack.append((left.left, right.right))
+            stack.append((left.right, right.left))
+        elif (not left and right) or (left and not right):
+            return False
+    return True
