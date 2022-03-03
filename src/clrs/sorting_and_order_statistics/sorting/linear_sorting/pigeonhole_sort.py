17d2413f554e2c6ac554f4f18030f37bdb20fe1e
zzhao30 <zzhao30@ford.com>
2022-03-03 10:10:49 -0500

    CCs.

--- src/clrs/sorting_and_order_statistics/sorting/linear_sorting/pigeonhole_sort.py
+++ src/clrs/sorting_and_order_statistics/sorting/linear_sorting/pigeonhole_sort.py
@@ -3,8 +3,8 @@ Overview
 ========
 
 Pigeonhole sorting is a sorting algorithm that is suitable for sorting lists of elements
-where the number of elements, n, and the length of the range of possible key values, N,
-are approximately the same. It requires O(n + N) time. It is similar to counting sort,
+where the number of elements, n, and the length of the range of possible key values, k,
+are approximately the same. It requires O(n + k) time. It is similar to counting sort,
 but differs in that it moves items twice: once to the bucket array and again to the
 final destination, whereas counting sort builds an auxiliary array and then uses the
 array to compute each item's final destination and moves the item there.
@@ -12,7 +12,7 @@ array to compute each item's final destination and moves the item there.
 The pigeonhole algorithm works as follows:
 
     1. Given an array of values to be sorted, set up an auxiliary array of initially
-empty "pigeonholes", one pigeonhole for each key in the range of the keys (N) in the
+empty "pigeonholes", one pigeonhole for each key in the range of the keys (k) in the
 original array.
     2. Going over the original array, put each value into the pigeonhole corresponding
 to its key, such that each pigeonhole eventually contains a list of all values with that
@@ -28,12 +28,12 @@ Complexity
 Time
 ----
 
-pigeonhole_sort(): O(n + N) worst case.
+pigeonhole_sort(): O(n + k) worst case.
 
 Space
 -----
 
-pigeonhole_sort(): O(n + N) for the pigeonholes array.
+pigeonhole_sort(): O(n + k) for the pigeonholes array.
 """
 
 
