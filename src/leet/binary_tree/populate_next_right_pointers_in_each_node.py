607b56a3157b30dbb13f7c9fc21bd5477f0619a0
zzhao30 <zzhao30@ford.com>
2022-03-03 11:21:43 -0500

    CCs.

---
+++ src/leet/binary_tree/populate_next_right_pointers_in_each_node.py
@@ -0,0 +1,46 @@
+"""
+Populating Next Right Pointers in Each Node II
+----------------------------------------------
+
+Given a binary tree, populate each next pointer to point to its next right node. If
+there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Complexity
+==========
+
+Time
+----
+
+connect(): O(n).
+
+Space
+-----
+
+connect(): O(1).
+"""
+
+
+def connect(root):
+    if not root:
+        return None
+    leftmost = root
+    while leftmost:
+        prev, node, leftmost = None, leftmost, None
+        while node:
+            if node.left:
+                prev, leftmost = update(prev, node.left, leftmost)
+            if node.right:
+                prev, leftmost = update(prev, node.right, leftmost)
+            node = node.next
+    return root
+
+
+def update(prev, node, leftmost):
+    if prev:
+        prev.next = node
+    else:
+        leftmost = node
+    prev = node
+    return prev, leftmost
