e390981aaa86cc68f4a96b504d79f302ca7a2237
FruVirus <zhen.zhao6@gmail.com>
2022-03-06 09:54:55 -0500

    CCs.

---
+++ src/leet/queue_and_stack/open_the_lock.py
@@ -0,0 +1,55 @@
+"""
+Open the Lock
+-------------
+
+You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots:
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap
+around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of
+turning one wheel one slot.
+
+The lock initially starts at '0000', a string representing the state of the 4 wheels.
+
+You are given a list of dead ends, meaning if the lock displays any of these codes, the
+wheels of the lock will stop turning and you will be unable to open it.
+
+Given a target representing the value of the wheels that will unlock the lock, return
+the minimum total number of turns required to open the lock, or -1 if it is impossible.
+
+Complexity
+==========
+
+Time
+----
+
+openLock(deadends, target): O(n^2 * a^n * d), where n is the number of digits in the
+lock, a is the number of digits in the alphabet, and d is the size of deadends.
+
+Space
+-----
+
+openLock(deadends, target): O(a^n + d).
+"""
+
+# Standard Library
+from collections import deque
+
+
+def sol(deadends, target):
+    def neighbors(combo):
+        for i in range(4):
+            x = int(combo[i])
+            for d in [-1, 1]:
+                yield combo[:i] + str((x + d) % 10) + combo[i + 1 :]
+
+    queue, visited = deque([("0000", 0)]), {"0000"}
+    while queue:
+        combo, num_turns = queue.popleft()
+        if combo == target:
+            return num_turns
+        if combo in deadends:
+            continue
+        for neighbor in neighbors(combo):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append((neighbor, num_turns + 1))
+    return -1
