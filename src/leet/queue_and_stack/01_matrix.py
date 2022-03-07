6dcb12481a969a13615f0d69b30b3115283fc458
zzhao30 <zzhao30@ford.com>
2022-03-07 16:46:03 -0500

    CCs.

---
+++ src/leet/queue_and_stack/01_matrix.py
@@ -0,0 +1,42 @@
+"""
+01 Matrix
+---------
+
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+Complexity
+==========
+
+Time
+----
+
+updateMatrix(mat): O(m * n).
+
+Space
+-----
+
+updateMatrix(mat): O(m * n).
+"""
+
+# Standard Library
+from collections import deque
+
+
+def sol(mat):
+    m, n = len(mat), len(mat[0])
+    dist, queue = [[float("inf")] * n for _ in range(m)], deque()
+    for row in range(m):
+        for col in range(n):
+            if mat[row][col] == 0:
+                dist[row][col] = 0
+                queue.append((row, col))
+    while queue:
+        row, col = queue.popleft()
+        for x, y in (-1, 0), (1, 0), (0, -1), (0, 1):
+            r, c = row + x, col + y
+            if 0 <= r < m and 0 <= c < n and dist[r][c] > dist[row][col] + 1:
+                dist[r][c] = dist[row][col] + 1
+                queue.append((r, c))
+    return dist
---
+++ src/leet/queue_and_stack/keys_and_rooms.py
@@ -0,0 +1,41 @@
+"""
+Keys and Rooms
+--------------
+
+There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room
+0. Your goal is to visit all the rooms. However, you cannot enter a locked room without
+having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number
+on it, denoting which room it unlocks, and you can take all of them with you to unlock
+the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you
+visited room i, return true if you can visit all the rooms, or false otherwise.
+
+Complexity
+==========
+
+Time
+----
+
+canVisitAllRooms(rooms): O(n + e), where n is the number of rooms and e is the total
+number of keys.
+
+Space
+-----
+
+canVisitAllRooms(rooms): O(n).
+"""
+
+
+def sol(rooms):
+    seen = [False] * len(rooms)
+    seen[0], stack = True, [0]
+    while stack:
+        room = stack.pop()
+        for keys in rooms[room]:
+            if not seen[keys]:
+                seen[keys] = True
+                stack.append(keys)
+    return all(seen)
