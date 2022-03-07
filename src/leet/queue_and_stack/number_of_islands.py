11ab2686f09d099cf25f8cdd9d386bf67c98a336
zzhao30 <zzhao30@ford.com>
2022-03-07 13:34:24 -0500

    CCs.

--- src/leet/queue_and_stack/number_of_islands.py
+++ src/leet/queue_and_stack/number_of_islands.py
@@ -2,7 +2,7 @@
 Number of Islands
 -----------------
 
-Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's
+Given an m x n 2D binary grid, grid, which represents a map of '1's (land) and '0's
 (water), return the number of islands.
 
 An island is surrounded by water and is formed by connecting adjacent lands horizontally
@@ -37,7 +37,7 @@ def sol(grid):
             queue = deque([(row, col)])
             while queue:
                 r, c = queue.popleft()
-                for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                     i, j = r + x, c + y
                     if 0 <= i < m and 0 <= j < n and grid[i][j] == "1":
                         grid[i][j] = "0"
