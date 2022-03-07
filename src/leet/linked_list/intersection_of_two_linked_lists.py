fd120db8533cb1f4079f7a17a0eeb2c4c6c0c2a7
zzhao30 <zzhao30@ford.com>
2022-03-07 13:08:49 -0500

    CCs.

--- src/leet/linked_list/intersection_of_two_linked_lists.py
+++ src/leet/linked_list/intersection_of_two_linked_lists.py
@@ -11,16 +11,16 @@ Complexity
 Time
 ----
 
-intersect(): O(n + m).
+getIntersectionNode(heada, headb): O(n + m).
 
 Space
 -----
 
-intersect(): O(1).
+getIntersectionNode(heada, headb): O(1).
 """
 
 
-def intersect(heada, headb):
+def sol(heada, headb):
     starta, startb = heada, headb
     while starta is not startb:
         starta = headb if not starta else starta.next
