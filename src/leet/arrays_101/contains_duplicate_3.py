d8b0539b19b430694981419b9f141ef14fbe83e5
zzhao30 <zzhao30@ford.com>
2022-03-10 11:48:29 -0500

    CCs.

--- src/leet/arrays_101/contains_duplicate_3.py
+++ src/leet/arrays_101/contains_duplicate_3.py
@@ -6,6 +6,27 @@ Given an integer array nums and two integers k and t, return true if there are t
 distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and
 abs(i - j) <= k.
 
+Intuition
+---------
+
+If we want the absolute difference between two numbers to be <= t, then this means we
+want the bucket windows to be of size w = t + 1 so that we can index into buckets using
+nums[i] // w.
+
+If t = 3, then the bucket windows are of size 4 and:
+
+    0 - 3 will go into bucket window 0.
+    4 - 7 will go into bucket window 1.
+    etc.
+
+Because of the organization, whenever a new number comes in, we only have to check the
+current bucket window and its adjacent neighbors to the left and right. All other bucket
+windows will be out of the range of t (i.e., it will contain numbers whose absolute
+difference with the new number will be greater than t).
+
+To take into account the condition abs(i - j) <= k, we use a sliding window where we
+delete the key at index nums[i - k] // w if i >= k.
+
 Complexity
 ==========
 
