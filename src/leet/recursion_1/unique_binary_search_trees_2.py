3cc9a3a849b2a37ad944a61c5509996a110bd1e9
zzhao30 <zzhao30@ford.com>
2022-03-11 12:07:02 -0500

    CCs.

---
+++ src/leet/recursion_1/unique_binary_search_trees_2.py
@@ -0,0 +1,43 @@
+"""
+K-th Symbol in Grammar
+----------------------
+
+Given an integer n, return all the structurally unique BST's (binary search trees),
+which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+Complexity
+==========
+
+Time
+----
+
+generateTrees(n): O().
+
+Space
+-----
+
+generateTrees(n): O().
+"""
+
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val, self.left, self.right = val, left, right
+
+
+def sol(n):
+    def generate_trees(start, end):
+        if start <= end:
+            all_trees = []
+            for i in range(start, end + 1):
+                left_trees = generate_trees(start, i - 1)
+                right_trees = generate_trees(i + 1, end)
+                for l in left_trees:
+                    for r in right_trees:
+                        tree = Node(i)
+                        tree.left, tree.right = l, r
+                        all_trees.append(tree)
+            return all_trees
+        return [None]
+
+    return generate_trees(1, n)
