9b5940fa0901aa636ab69a4881c31a816fe6f1ce
FruVirus <zhen.zhao6@gmail.com>
2022-03-10 20:19:15 -0500

    CCs.

--- src/leet/binary_search_tree/validate_binary_search_tree.py
+++ src/leet/binary_search_tree/validate_binary_search_tree.py
@@ -1,6 +1,6 @@
 """
-Find K Closest Elements
------------------------
+Validate Binary Search Tree
+---------------------------
 
 Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
