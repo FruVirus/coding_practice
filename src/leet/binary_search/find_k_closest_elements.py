aa27c56275e71fd42b328b73d8cfab576f392c74
zzhao30 <zzhao30@ford.com>
2022-03-12 14:17:51 -0500

    CCs.

--- src/leet/binary_search/find_k_closest_elements.py
+++ src/leet/binary_search/find_k_closest_elements.py
@@ -5,6 +5,22 @@ Find K Closest Elements
 Given a sorted integer array arr, two integers k and x, return the k closest integers to
 x in the array. The result should also be sorted in ascending order.
 
+Intuition
+---------
+
+We need to find the leftmost index of a number that will be in the final answer. The
+farthest right it could possibly be is the length of the array - k. Otherwise, there
+wouldn't be enough elements to its right.
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8], k = 3, x = 4
+
+low = 0, high = 5, mid = 2, mid + k = 5
+
+Since arr[mid] (3) is closer to x than arr[mid + k] (6), this means that arr[mid + k:]
+(6, 7, 8) can never be in the final answer, since that would imply 3 is not in the final
+answer but we know that 3 is closer to x than 6, 7, and 8. Thus, we move the high
+pointer.
+
 Complexity
 ==========
 
