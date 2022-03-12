b4d86deb1c746c4891f44026420c6075d92d1584
zzhao30 <zzhao30@ford.com>
2022-03-12 14:36:19 -0500

    CCs.

--- src/leet/binary_search/find_minimum_in_rotated_sorted_array_2.py
+++ src/leet/binary_search/find_minimum_in_rotated_sorted_array_2.py
@@ -58,10 +58,10 @@ def sol(nums):
     low, high = 0, len(nums) - 1
     while low < high:
         mid = low + (high - low) // 2
-        if nums[mid] < nums[high]:
-            high = mid
-        elif nums[mid] > nums[high]:
+        if nums[mid] > nums[high]:
             low = mid + 1
+        elif nums[mid] < nums[high]:
+            high = mid
         else:
             high -= 1
     return nums[low]
