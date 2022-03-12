cd6e1fee863e90c07e6c2f8426f15a8fd3f0c7e6
zzhao30 <zzhao30@ford.com>
2022-03-12 14:30:44 -0500

    CCs.

--- src/leet/binary_search/find_minimum_in_rotated_sorted_array_2.py
+++ src/leet/binary_search/find_minimum_in_rotated_sorted_array_2.py
@@ -16,6 +16,28 @@ element of this array.
 
 You must decrease the overall operation steps as much as possible.
 
+Intuition
+---------
+
+In this problem, the numbers do not have to be unique; thus, it is a more general
+problem.
+
+This problem needs to address three cases:
+
+    1. nums[mid] < nums[high]: The mid element resides in the same half as the high
+element. Therefore, the desired minimum element should reside to the left-hand side of
+the mid element. As a result, we then move high down to the mid index.
+
+    2. nums[mid] > nums[high]: THe mid element resides in the different half of the
+array as the high element. Therefore, the desired minimum element should reside in the
+right-hand side of the mid element. As a result, we move the low index one past the mid
+index.
+
+    3. nums[mid] == nums[high]: In this case, we are not sure which side of the mid
+element the desired minimum resides in. To further reduce the search scope, a safe
+measure would be to reduce the high index by one rather than aggressively moving the mid
+index.
+
 Complexity
 ==========
 
@@ -36,11 +58,10 @@ def sol(nums):
     low, high = 0, len(nums) - 1
     while low < high:
         mid = low + (high - low) // 2
-        if nums[low] == nums[mid] == nums[high]:
-            low += 1
-            high -= 1
-        elif nums[mid] <= nums[high]:
+        if nums[mid] < nums[high]:
             high = mid
-        else:
+        elif nums[mid] > nums[high]:
             low = mid + 1
+        else:
+            high -= 1
     return nums[low]
