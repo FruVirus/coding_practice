b51fb680f9148a494fd76ad5d0f4cad086ec6f64
FruVirus <zhen.zhao6@gmail.com>
2022-03-13 16:51:49 -0400

    CCs.

--- src/leet/queue_and_stack/01_matrix.py
+++ src/leet/queue_and_stack/01_matrix.py
@@ -17,7 +17,7 @@ updateMatrix(mat): O(m * n).
 Space
 -----
 
-updateMatrix(mat): O(m * n).
+updateMatrix(mat): O(1).
 """
 
 # Standard Library
@@ -25,18 +25,18 @@ from collections import deque
 
 
 def sol(mat):
-    m, n = len(mat), len(mat[0])
-    dist, queue = [[float("inf")] * n for _ in range(m)], deque()
+    m, n, queue = len(mat), len(mat[0]), deque()
     for row in range(m):
         for col in range(n):
             if mat[row][col] == 0:
-                dist[row][col] = 0
                 queue.append((row, col))
+            else:
+                mat[row][col] = float("inf")
     while queue:
         row, col = queue.popleft()
         for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             r, c = row + x, col + y
-            if 0 <= r < m and 0 <= c < n and dist[r][c] > dist[row][col]:
-                dist[r][c] = dist[row][col] + 1
+            if 0 <= r < m and 0 <= c < n and mat[r][c] > mat[row][col]:
+                mat[r][c] = mat[row][col] + 1
                 queue.append((r, c))
-    return dist
+    return mat
