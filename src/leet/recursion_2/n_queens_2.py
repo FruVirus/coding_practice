22d6fd3e130340e34b3e389bb2dc50ad61c6d03d
zzhao30 <zzhao30@ford.com>
2022-03-15 15:23:39 -0400

    CCs.

--- src/leet/recursion_2/n_queens_2.py
+++ src/leet/recursion_2/n_queens_2.py
@@ -18,15 +18,16 @@ totalNQueens(n): O(n!).
 Space
 -----
 
-totalNQueens(n): O(n).
+totalNQueens(n): O(n^2).
 """
 
 
 def sol(n):
-    def backtrack(row, cols, diags, anti_diags):
+    def backtrack(row, cols, diags, anti_diags, board):
         if row == n:
+            sol.append(["".join(row) for row in board])
             return 1
-        sol = 0
+        count = 0
         for col in range(n):
             curr_diag, curr_anti_diag = row - col, row + col
             if col in cols or curr_diag in diags or curr_anti_diag in anti_diags:
@@ -34,10 +35,14 @@ def sol(n):
             cols.add(col)
             diags.add(curr_diag)
             anti_diags.add(curr_anti_diag)
-            sol += backtrack(row + 1, cols, diags, anti_diags)
+            board[row][col] = "Q"
+            count += backtrack(row + 1, cols, diags, anti_diags, board)
             cols.remove(col)
             diags.remove(curr_diag)
             anti_diags.remove(curr_anti_diag)
-        return sol
+            board[row][col] = "."
+        return count
 
-    return backtrack(0, set(), set(), set())
+    sol, board = [], [["."] * n for _ in range(n)]
+    count = backtrack(0, set(), set(), set(), board)
+    return count, sol
