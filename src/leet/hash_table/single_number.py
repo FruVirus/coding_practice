39f1c534eadd2a8f5c27f94196ef9f8a891974c5
zzhao30 <zzhao30@ford.com>
2022-03-17 15:00:08 -0400

    CCs.

---
+++ src/leet/hash_table/single_number.py
@@ -0,0 +1,45 @@
+"""
+Single Number
+-------------
+
+Given a non-empty array of integers nums, every element appears twice except for one.
+Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant
+extra space.
+
+Intuition
+---------
+
+If we take XOR of zero and some bit, it will return that bit.
+
+If we take XOR of two same bits, it will return 0.
+
+a XOR b XOR a = (a XOR a) XOR b = 0 XOR b = b
+
+So we can XOR all bits together to find the unique number.
+
+nums = [4, 1, 2, 1, 2]
+
+
+
+Complexity
+==========
+
+Time
+----
+
+singleNumber(nums): O(n).
+
+Space
+-----
+
+singleNumber(nums): O(1).
+"""
+
+
+def sol(nums):
+    a = 0
+    for num in nums:
+        a ^= num
+    return a
