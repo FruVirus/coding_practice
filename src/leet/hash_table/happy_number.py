853e05ff24cfb453e5498ae665329ea4e332b1c7
zzhao30 <zzhao30@ford.com>
2022-03-17 15:32:38 -0400

    CCs.

--- src/leet/hash_table/happy_number.py
+++ src/leet/hash_table/happy_number.py
@@ -14,23 +14,44 @@ endlessly in a cycle which does not include 1.
 
 Return true if n is a happy number, and false if not.
 
+Intuition
+---------
+
+Instead of keeping track of just one value in the chain, we keep track of 2, called the
+slow runner and the fast runner. At each step of the algorithm, the slow runner goes
+forward by 1 number in the chain, and the fast runner goes forward by 2 numbers (nested
+calls to the getNext(n) function).
+
+If n is a happy number, i.e. there is no cycle, then the fast runner will eventually get
+to 1 before the slow runner.
+
+If n is not a happy number, then eventually the fast runner and the slow runner will be
+on the same number.
+
 Complexity
 ==========
 
 Time
 ----
 
-singleNumber(nums): O(n).
+isHappy(n): O(lg n).
 
 Space
 -----
 
-singleNumber(nums): O(1).
+isHappy(n): O(1).
 """
 
 
-def sol(nums):
-    a = 0
-    for num in nums:
-        a ^= num
-    return a
+def sol(n):
+    def get_next(num):
+        sum_ = 0
+        while num > 0:
+            num, digit = num // 10, num % 10
+            sum_ += digit ** 2
+        return sum_
+
+    slow, fast = n, get_next(n)
+    while slow != fast and fast != 1:
+        slow, fast = get_next(slow), get_next(get_next(fast))
+    return fast == 1
