f322c00c14ddce9e8f87ee63d04cd9c869b74836
zzhao30 <zzhao30@ford.com>
2022-03-20 16:14:39 -0400

    CCs.

--- src/leet/queue_and_stack/maximum_frequency_stack.py
+++ src/leet/queue_and_stack/maximum_frequency_stack.py
@@ -59,14 +59,14 @@ class Sol:
     def __init__(self):
         self.freq, self.group, self.max_freq = Counter(), defaultdict(list), 0
 
-    def push(self, val):
-        self.freq[val] += 1
-        self.max_freq = max(self.max_freq, self.freq[val])
-        self.group[self.freq[val]].append(val)
-
     def pop(self):
         x = self.group[self.max_freq].pop()
         self.freq[x] -= 1
         if not self.group[self.max_freq]:
             self.max_freq -= 1
         return x
+
+    def push(self, val):
+        self.freq[val] += 1
+        self.max_freq = max(self.max_freq, self.freq[val])
+        self.group[self.freq[val]].append(val)
