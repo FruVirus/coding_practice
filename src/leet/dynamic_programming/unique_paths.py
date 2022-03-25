320244493513ceb499e6627c226df37d4a05845b
FruVirus <zhen.zhao6@gmail.com>
2022-03-25 16:35:39 -0400

    CCs.

--- src/leet/dynamic_programming/unique_paths.py
+++ src/leet/dynamic_programming/unique_paths.py
@@ -12,6 +12,40 @@ robot can take to reach the bottom-right corner.
 Intuition
 ---------
 
+1. An array that answers the problem for a given state
+
+State variables are usually easy to find in pathing problems. Similar to how we need one
+index (i) for 1D array inputs, with pathing problems on a 2D matrix, we need two indices
+(row and col) to denote position. Some problems have added constraints that will require
+additional state variables, but there doesn't seem to be anything of the sort in this
+problem. Therefore, we will just use two state variables row which represents the
+current row, and col which represents the current column.
+
+The problem is asking for the number of paths to the final square, so let's have
+dp[row][col] represent how many paths there are from the start (top-left corner) to the
+square at (row, col). We will return dp[m - 1][n - 1] where m and n are the number of
+rows and columns respectively.
+
+2. A recurrence relation to transition between states
+
+The problem says that we are allowed to move down or right. That means, if we are at
+some square, we arrived from either the square above or the square to the left. These
+two squares are (row - 1, col) and (row, col - 1). Since we can arrive at the current
+square from either of these squares, the number of ways to get to the current square is
+the sum of the number of ways to get to these two squares. Either of these may be out of
+the grid bounds, so we should make sure to check for that. This gives us our simple
+recurrence relation:
+
+    - dp[row][col] = dp[row - 1][col] + dp[row][col - 1], where
+dp[row - 1][col] and dp[row][col - 1] is equal to 0 if out of bounds.
+
+3. Base cases
+
+In the previous chapter, when talking about counting DP problems, we said that the base
+cases need to be set to nonzero values so that the terms in the recurrence relation
+don't just stay stuck at zero. In this problem, we start in the top-left corner. How
+many ways are there for us to get to the first square? Only 1 - we start on it.
+Therefore, our base case is dp[0][0] = 1.
 
 Complexity
 ==========
@@ -19,18 +53,39 @@ Complexity
 Time
 ----
 
-uniquePaths(m, n): O().
+uniquePaths(m, n): O(m * n).
 
 Space
 -----
 
-uniquePaths(m, n): O().
+uniquePaths_bu(m, n): O(n).
+uniquePaths_td(m, n): O(m * n).
 """
 
 
 def sol_bu(m, n):
-    pass
+    if m > n:
+        m, n = n, m
+    dp = [1] * m
+    for _ in range(1, n):
+        for i in range(1, m):
+            dp[i] += dp[i - 1]
+    return dp[-1]
 
 
 def sol_td(m, n):
-    pass
+    memo = {}
+
+    def dp(row, col):
+        if row == col == 0:
+            return 1
+        if (row, col) not in memo:
+            num_paths = 0
+            if row > 0:
+                num_paths += dp(row - 1, col)
+            if col > 0:
+                num_paths += dp(row, col - 1)
+            memo[(row, col)] = num_paths
+        return memo[(row, col)]
+
+    return dp(m - 1, n - 1)
