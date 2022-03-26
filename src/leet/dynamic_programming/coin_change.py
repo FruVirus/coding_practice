07c158ab8d3c1f103bfa8d9e4f7a8fb5aff4d510
zzhao30 <zzhao30@ford.com>
2022-03-26 15:50:09 -0400

    CCs.

--- src/leet/dynamic_programming/coin_change.py
+++ src/leet/dynamic_programming/coin_change.py
@@ -72,7 +72,7 @@ def sol_bu(coins, amount):
     for coin in coins:
         for i in range(coin, amount + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[amount] if dp[amount] != float("inf") else -1
+    return dp[-1] if dp[-1] != float("inf") else -1
 
 
 def sol_td(coins, amount):
