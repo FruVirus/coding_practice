307b7709e9d5d1e01fc96b34410929c84b932c14
zzhao30 <zzhao30@ford.com>
2022-03-25 22:15:14 -0400

    CCs.

--- src/leet/arrays_101/move_zeros.py
+++ src/leet/arrays_101/move_zeros.py
@@ -27,9 +27,9 @@ moveZeroes(nums): O(1).
 
 def sol(nums):
     last_non_zero = 0
-    for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[last_non_zero] = nums[i]
+    for num in nums:
+        if num != 0:
+            nums[last_non_zero] = num
             last_non_zero += 1
     for i in range(last_non_zero, len(nums)):
         nums[i] = 0
