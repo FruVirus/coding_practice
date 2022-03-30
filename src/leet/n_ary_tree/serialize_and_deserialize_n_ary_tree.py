685f7499f012f323e7c6ceee147e62350f4d4fa9
zzhao30 <zzhao30@ford.com>
2022-03-30 11:29:40 -0400

    CCs.

--- src/leet/n_ary_tree/serialize_and_deserialize_n_ary_tree.py
+++ src/leet/n_ary_tree/serialize_and_deserialize_n_ary_tree.py
@@ -13,6 +13,11 @@ how your serialization/deserialization algorithm should work. You just need to e
 that an N-ary tree can be serialized to a string and this string can be deserialized to
 the original tree structure.
 
+Intuition
+---------
+
+We add a sentinel value when all the children have been added to the final string.
+
 Complexity
 ==========
 
@@ -28,30 +33,49 @@ serialize(root) and deserialize(data): O(n).
 """
 
 
+class WrappableInt:
+    def __init__(self, x):
+        self.value = x
+
+    def get_value(self):
+        return self.value
+
+    def increment(self):
+        self.value += 1
+
+
 class Node:
     def __init__(self, val=None, children=None):
         self.val, self.children = val, children
 
 
 def sol_serialize(root):
-    if not root:
-        return ""
+    sol = []
 
-    def serialize_(root):
-        return [[node.val, serialize_(node)] for node in root.children]
+    def serialize_(root, sol):
+        if not root:
+            return
+        sol.append(chr(root.val + 36))
+        for child in root.children:
+            serialize_(child, sol)
+        sol.append("#")
 
-    return [root.val, serialize_(root)]
+    serialize_(root, sol)
+    return "".join(sol)
 
 
 def sol_deserialize(data):
     if not data:
         return None
 
-    def deserialize_(l):
-        root = l.pop(0)
-        for sub_l in l:
-            node = Node(root)
-            node.children = [deserialize_(x) for x in sub_l]
-            return node
+    def deserialize_(data, index):
+        if index.get_value() == len(data):
+            return None
+        node = Node(ord(data[index.get_value()]) - 36, [])
+        index.increment()
+        while data[index.get_value()] != "#":
+            node.children.append(deserialize_(data, index))
+        index.increment()
+        return node
 
-    return deserialize_(data)
+    return deserialize_(data, WrappableInt(0))
