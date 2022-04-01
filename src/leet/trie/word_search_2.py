8aa1b7807527cae488b74971f7ccc129dbce1b98
zzhao30 <zzhao30@ford.com>
2022-04-01 11:52:57 -0400

    CCs.

---
+++ src/leet/trie/word_search_2.py
@@ -0,0 +1,139 @@
+"""
+Word Search II
+--------------
+
+Given an m x n board of characters and a list of strings words, return all words on the
+board.
+
+Each word must be constructed from letters of sequentially adjacent cells, where
+adjacent cells are horizontally or vertically neighboring. The same letter cell may not
+be used more than once in a word.
+
+Intuition
+---------
+
+The problem is actually a simplified crossword puzzle game, where the word solutions
+have been given on the board embedded with some noise letters. All we need to to do is
+to cross them out.
+
+Intuitively, in order to cross out all potential words, the overall strategy would be to
+iterate the cell one by one, and from each cell we walk along its neighbors in four
+potential directions to find matched words. While wandering around the board, we would
+stop the exploration when we know it would not lead to the discovery of new words.
+
+The key of the solution lies on how we find the matches of word from the dictionary.
+Intuitively, one might resort to the hashset data structure (e.g. set() in Python). This
+could work.
+
+However, during the backtracking process, one would encounter more often the need to
+tell if there exists any word that contains certain prefix, rather than if a string
+exists as a word in the dictionary. Because if we know that there does not exist any
+match of word in the dictionary for a given prefix, then we would not need to further
+explore certain direction. And this, would greatly reduce the exploration space,
+therefore improve the performance of the backtracking algorithm.
+
+The overall workflow of the algorithm is intuitive, which consists of a loop over each
+cell in the board and a recursive function call starting from the cell. Here is the
+skeleton of the algorithm.
+
+    - We build a Trie out of the words in the dictionary, which would be used for the
+matching process later.
+
+    - Starting from each cell, we start the backtracking exploration (i.e.,
+backtracking(cell)), if there exists any word in the dictionary that starts with the
+letter in the cell.
+
+    - During the recursive function call backtracking(cell), we explore the neighbor
+cells (i.e., neighborCell) around the current cell for the next recursive call
+backtracking(neighborCell). At each call, we check if the sequence of letters that we
+traverse so far matches any word in the dictionary, with the help of the Trie data
+structure that we built at the beginning.
+
+For improvements, we can gradually prune the nodes in Trie during the backtracking.
+
+    - The idea is motivated by the fact that the time complexity of the overall
+algorithm sort of depends on the size of the Trie. For a leaf node in Trie, once we
+traverse it (i.e., find a matched word), we would no longer need to traverse it again.
+As a result, we could prune it out from the Trie.
+
+    - Gradually, those non-leaf nodes could become leaf nodes later, since we trim their
+children leaf nodes. In the extreme case, the Trie would become empty, once we find a
+match for all the words in the dictionary. This pruning measure could reduce up to 50%
+of the running time for the test cases of the online judge.
+
+For example:
+
+["oath","pea","eat","rain"]
+
+{
+    'o': {
+    'a': {
+    't': {
+    'h': {'$': 'oath'}}}},
+
+    'p': {
+    'e': {
+    'a': {'$': 'pea'}}},
+
+    'e': {
+    'a': {
+    't': {'$': 'eat'}}},
+
+    'r': {
+    'a': {
+    'i': {
+    'n': {'$': 'rain'}}}}
+}
+
+Complexity
+==========
+
+Time
+----
+
+findWords(board, words): O(m * (4 * 3^(l - 1)), where m is the number of cells in the
+board and l is the maximum length of words. Assume the maximum length of word is L,
+starting from a cell, initially we would have at most 4 directions to explore. Assume
+each direction is valid (i.e., worst case), during the following exploration, we have at
+most 3 neighbor cells (excluding the cell where we come from) to explore. As a result,
+we would traverse at most 4 * 3^(L − 1) cells during the backtracking exploration.
+
+Space
+-----
+
+findWords(board, words): O(n), where n is the total number of letters in the dictionary.
+"""
+
+
+def sol(board, words):
+    m, n, sol, trie = len(board), len(board[0]), [], build_trie(words)
+
+    def backtrack(i, j, node):
+        char, parent = board[i][j], node
+        if char in node:
+            board[i][j] = "#"
+            node = node[char]
+            if "$" in node:
+                sol.append(node["$"])
+                del node["$"]
+            for r, c in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+                if 0 <= r < m and 0 <= c < n:
+                    backtrack(r, c, node)
+            board[i][j] = char
+            if not node:
+                parent.pop(char)
+
+    for i in range(m):
+        for j in range(n):
+            backtrack(i, j, trie)
+    return sol
+
+
+def build_trie(words):
+    trie = {}
+    for word in words:
+        node = trie
+        for char in word:
+            node = node.setdefault(char, {})
+        node["$"] = word
+    return trie
---
+++ src/leet/trie/word_squares.py
@@ -0,0 +1,145 @@
+"""
+Word Squares
+------------
+
+Given an array of unique strings words, return all the word squares you can build from
+words. The same word from words can be used multiple times. You can return the answer in
+any order.
+
+A sequence of strings forms a valid word square if the k-th row and column read the same
+string, where 0 <= k < max(numRows, numColumns).
+
+    - For example, the word sequence ["ball","area","lead","lady"] forms a word square
+because each word reads the same both horizontally and vertically.
+
+Intuition
+---------
+
+For a word square with equal-sized row and column, the resulting letter matrix should be
+symmetrical across its diagonal.
+
+In other words, if we know the upper-right part of the word square, we could infer its
+lower-left part, and vice versa.
+
+The idea is that we construct the word square row by row from top to down. At each row,
+we simply do trial and error; i.e., we try with one word, if it does not meet the
+constraint then we try another one. At the end, if one repeats the above steps with each
+word as the starting word, one would exhaust all the possibilities to construct a valid
+word square.
+
+Note that, we tweak the Trie data structure a bit, in order to further optimize the time
+and space complexity.
+
+    - Instead of labeling the word at the leaf node of the Trie, we label the word at
+each node so that we don't need to perform a further traversal once we reach the last
+node in the prefix. This trick could help us with the time complexity.
+
+    - Instead of storing the actual words in the Trie, we keep only the index of the
+word, which could greatly save the space.
+
+For example:
+
+["area","lead","wall","lady","ball"]
+
+{
+    'a': {
+    '#': [0],
+    'r': {
+    '#': [0],
+    'e': {
+    '#': [0],
+    'a': {'#': [0]}}}},
+
+    'l': {
+        '#': [1, 3],
+        'e': {
+        '#': [1],
+        'a': {
+        '#': [1],
+        'd': {'#': [1]}}},
+
+        'a': {
+        '#': [3],
+        'd': {
+        '#': [3],
+        'y': {'#': [3]}}}
+    },
+
+    'w': {
+    '#': [2],
+    'a': {
+    '#': [2],
+    'l': {
+    '#': [2],
+    'l': {
+    '#': [2]}}}},
+
+    'b': {
+    '#': [4],
+    'a': {
+    '#': [4],
+    'l': {
+    '#': [4],
+    'l': {
+    '#': [4]}}}}
+}
+
+Complexity
+==========
+
+Time
+----
+
+wordSquares(words): O(n * 26^l * l), where n is the number of input words and l is the
+length of a single word.
+
+Space
+-----
+
+wordSquares(words): O(n * l + n * l / 2) = O(n * l). The first half of the space
+complexity (i.e., n * l) is the word indices we store in the Trie, where we store l
+times the index for each word. The second half (i.e., n * l / 2) is the space for the
+prefixes of all words (in the worst case, we have no overlapping prefixes).
+"""
+
+
+def sol(words):
+    sol, trie = [], build_trie(words)
+
+    def backtrack(step, word_squares):
+        if step == len(words[0]):
+            sol.append(word_squares[:])
+            return
+        prefix = "".join([word[step] for word in word_squares])
+        for word in starts_with(prefix, words, trie):
+            word_squares.append(word)
+            backtrack(step + 1, word_squares)
+            word_squares.pop()
+
+    for word in words:
+        backtrack(1, [word])
+    return sol
+
+
+def build_trie(words):
+    trie = {}
+    for i, word in enumerate(words):
+        node = trie
+        for char in word:
+            if char in node:
+                node = node[char]
+            else:
+                new_node = {"#": []}
+                node[char] = new_node
+                node = new_node
+            node["#"].append(i)
+    return trie
+
+
+def starts_with(prefix, words, trie):
+    node = trie
+    for char in prefix:
+        if char not in node:
+            return []
+        node = node[char]
+    return [words[i] for i in node["#"]]
