48629176d4a179c05b7803402b8cbe3266442ce6
FruVirus <zhen.zhao6@gmail.com>
2022-04-01 16:08:40 -0400

    CCs.

--- src/leet/dynamic_programming/delete_and_earn.py
+++ src/leet/dynamic_programming/delete_and_earn.py
@@ -97,8 +97,7 @@ def sol_bu(nums):
     max_num, points = max(nums), defaultdict(int)
     for num in nums:
         points[num] += num
-    max_points = [0] * (max_num + 1)
-    max_points[1] = points[1]
+    max_points = [0] + [points[1]] + [0] * (max_num - 1)
     for num in range(2, len(max_points)):
         max_points[num] = max(max_points[num - 1], max_points[num - 2] + points[num])
     return max_points[max_num]
