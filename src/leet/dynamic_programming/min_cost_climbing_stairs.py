6ba86e932101c1f5ab5d380ceeacd6f515cfd87d
FruVirus <zhen.zhao6@gmail.com>
2022-04-03 14:08:22 -0400

    CCs.

--- src/leet/dynamic_programming/min_cost_climbing_stairs.py
+++ src/leet/dynamic_programming/min_cost_climbing_stairs.py
@@ -36,7 +36,7 @@ def sol_td(cost):
     memo = {}
 
     def dp(i):
-        if i <= 1:
+        if i < 2:
             return 0
         if i not in memo:
             memo[i] = min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2])
