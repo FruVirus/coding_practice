e7229d7a4e24372dac7dc61dd7df6fb808a27634
zzhao30 <zzhao30@ford.com>
2022-04-04 11:30:18 -0400

    CCs.

--- src/leet/dynamic_programming/unique_paths.py
+++ src/leet/dynamic_programming/unique_paths.py
@@ -68,8 +68,8 @@ def sol_bu(m, n):
         m, n = n, m
     dp = [1] * m
     for _ in range(1, n):
-        for i in range(1, m):
-            dp[i] += dp[i - 1]
+        for j in range(1, m):
+            dp[j] += dp[j - 1]
     return dp[-1]
 
 
