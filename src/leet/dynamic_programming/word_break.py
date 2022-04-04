a54184b975d2613c01c3354b0ecdbb8705e1cc7c
zzhao30 <zzhao30@ford.com>
2022-04-04 11:59:40 -0400

    CCs.

--- src/leet/dynamic_programming/word_break.py
+++ src/leet/dynamic_programming/word_break.py
@@ -11,19 +11,10 @@ segmentation.
 Intuition
 ---------
 
-In summary, the criteria is:
-
-    1. A word from wordDict can end at the current index i.
-
-    2. If that word is to end at index i, then it starts at index i - word.length + 1.
-The index before that i - word.length should also be formable from wordDict.
-
-The base case for this problem is another simple one. The first word used from wordDict
-starts at index 0, which means we would need to check dp[-1] for the second criteria,
-which is out of bounds. To fix this, we say that the second criteria can also be
-satisfied by i == word.length - 1.
-
-In the top-down approach, we can check for the base case by returning true if i < 0.
+For the recursive approach, we check every possible prefix of the string in the
+dictionary of words---if it is found in the dictionary, then the recursive function is
+called for the remaining portion of the string. And, if in some function call the whole
+string is found in the dictionary, then it will return True.
 
 Complexity
 ==========
@@ -43,13 +34,12 @@ wordBreak(s, word_dict): O(n).
 
 
 def sol_bu(s, word_dict):
-    dp = [False] * len(s)
-    for i in range(len(s)):
-        for word in word_dict:
-            if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
-                if s[i - len(word) + 1 : i + 1] == word:
-                    dp[i] = True
-                    break
+    dp = [True] + [False] * len(s)
+    for i in range(1, len(s) + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in word_dict:
+                dp[i] = True
+                break
     return dp[-1]
 
 
@@ -57,16 +47,14 @@ def sol_td(s, word_dict):
     memo = {}
 
     def dp(i):
-        if i < 0:
+        if i == len(s):
             return True
         if i not in memo:
-            for word in word_dict:
-                if i >= len(word) - 1 and dp(i - len(word)):
-                    if s[i - len(word) + 1 : i + 1] == word:
-                        memo[i] = True
-                        break
-            else:
-                memo[i] = False
+            memo[i] = False
+            for j in range(i + 1, len(s) + 1):
+                if s[i:j] in word_dict and dp(j):
+                    memo[i] = True
+                    break
         return memo[i]
 
-    return dp(len(s) - 1)
+    return dp(0)
