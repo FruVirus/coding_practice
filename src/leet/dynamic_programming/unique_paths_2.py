fff0f2552c78712014b19dd351bce7ea902fda67
zzhao30 <zzhao30@ford.com>
2022-04-09 10:59:30 -0400

    CCs.

--- src/leet/dynamic_programming/unique_paths_2.py
+++ src/leet/dynamic_programming/unique_paths_2.py
@@ -31,7 +31,7 @@ uniquePathsWithObstacles_td(grid): O(m * n).
 def sol_bu(grid):
     m, n = len(grid), len(grid[0])
     dp = [1 - i for i in grid[0]]
-    for i in range(1, len(dp)):
+    for i in range(1, n):
         if dp[i - 1] == 0:
             dp[i] = 0
     for i in range(1, m):
