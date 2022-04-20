1f7fc83030ee9a5c04221587c70afc00d08a38e1
zzhao30 <zzhao30@ford.com>
2022-04-20 14:30:25 -0400

    CCs.

--- src/leet/__init__.py
+++ src/leet/__init__.py
@@ -7,14 +7,14 @@ binary_tree:            8
 --------------------------
 dynamic_programming:    28
 --------------------------
-graph:                  21
+graph:                  20
 --------------------------
 hash_table:             18
 heap:                   8
 linked_list:            14
 --------------------------
 n_ary_tree:             4
-queue_and_stack:        18
+queue_and_stack:        19
 recursion_1:            4
 recursion_2:            10
 trie:                   9
