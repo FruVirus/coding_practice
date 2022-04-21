77f482742ec5a4de1ab26b45c5d8ad2e2d710e0f
zzhao30 <zzhao30@ford.com>
2022-04-21 11:01:16 -0400

    CCs.

--- src/leet/recursion_1/fibonacci_number.py
+++ src/leet/recursion_1/fibonacci_number.py
@@ -26,8 +26,7 @@ fib(n): O(n).
 def sol(n):
     if n < 2:
         return n
-    one, two, curr = 1, 0, 0
+    one, two = 1, 0
     for _ in range(2, n + 1):
-        curr = one + two
-        one, two = curr, one
-    return curr
+        one, two = one + two, one
+    return one
