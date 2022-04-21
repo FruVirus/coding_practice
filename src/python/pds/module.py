d8c16f4c051f2f89057d8df7b28b8a79c084d407
zzhao30 <zzhao30@ford.com>
2022-04-21 16:59:45 -0400

    CCs.

---
+++ src/python/pds/module.py
@@ -0,0 +1,12 @@
+# pylint: disable=C0114, E1121, R0204
+
+# Third Party Library
+import pandas as pd
+
+# Pandas Tutorial #
+
+# Pandas Getting Started
+print(pd.__version__)
+mydataset = {"cars": ["BMW", "Volvo", "Ford"], "passings": [3, 7, 2]}
+myvar = pd.DataFrame(mydataset)
+print(myvar)
