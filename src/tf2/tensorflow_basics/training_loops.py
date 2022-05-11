d646ef31ea884d8e5b30de877d28e3e02ea61ed8
zzhao30 <zzhao30@ford.com>
2022-05-10 22:00:49 -0400

    CCs.

---
+++ src/tf2/tensorflow_basics/training_loops.py
@@ -0,0 +1,17 @@
+# pylint: disable=C0114, E1123, E1120, E0102, W0223, R0901, W0703, W0613, R0204
+
+# Third Party Library
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+# Data #
+
+# Define the model #
+
+# Define a loss function
+
+# Define a training loop
+
+# The same solution, but with Keras #
