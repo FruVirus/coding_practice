97a19aa6627a8194e0d9fe1f5f347814656a2c79
FruVirus <zhen.zhao6@gmail.com>
2022-05-12 12:00:08 -0400

    CCs.

--- src/leet/__init__.py
+++ src/leet/__init__.py
@@ -13,8 +13,8 @@ recursion_1:            4
 hash_table:             18
 heap:                   8
 linked_list:            14
---------------------------
 n_ary_tree:             4
+--------------------------
 queue_and_stack:        19
 recursion_2:            10
 trie:                   9
