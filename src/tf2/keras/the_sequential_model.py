2ab3de9e9ad7b5b39f606219718ec565aef0de7b
zzhao30 <zzhao30@ford.com>
2022-05-12 20:52:58 -0400

    CCs.

---
+++ src/tf2/keras/the_sequential_model.py
@@ -0,0 +1,22 @@
+# pylint: disable=C0114, E1123, E1120, E0102, W0223, R0901, W0703, W0613, R0204
+# pylint: disable=E1129, C0102, W0603
+
+# Third Party Library
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# When to use a Sequential model #
+
+# Creating a Sequential model #
+
+# Specifying the input shape in advance #
+
+# A common debugging workflow: add() + summary() #
+
+# What to do once you have a model #
+
+# Feature extraction with a Sequential model #
+
+# Transfer learning with a Sequential model #
