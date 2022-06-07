0e76fd97cc9f65ced9d73d0bf573ce64f4d0d31f
zzhao30 <zzhao30@ford.com>
2022-06-07 17:13:13 -0400

    CCs.

--- src/leet/arrays_101/missing_number.py
+++ src/leet/arrays_101/missing_number.py
@@ -5,6 +5,10 @@ Missing Number
 Given an array nums containing n distinct numbers in the range [0, n], return the only
 number in the range that is missing from the array.
 
+Note that you can also use Gauss' formula:
+
+Expected sum = len(nums) * (len(nums) + 1) // 2 - sum(nums)
+
 Complexity
 ==========
 
@@ -21,4 +25,5 @@ missingNumber(nums): O(1).
 
 
 def sol(nums):
-    return len(nums) * (len(nums) + 1) // 2 - sum(nums)
+    actual_sum, expected_sum = sum(nums), sum(i for i in range(1, len(nums) + 1))
+    return expected_sum - actual_sum
