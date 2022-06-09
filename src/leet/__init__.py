1a5d6be0ec62a3fb182db63a7a978c83ec11f798
FruVirus <zhen.zhao6@gmail.com>
2022-06-08 21:24:00 -0400

    CCs.

--- src/leet/__init__.py
+++ src/leet/__init__.py
@@ -1,9 +1,9 @@
 """
 array_and_string:       14
-arrays_101:             17
+arrays_101:             18
 binary_search:          11
 binary_search_tree:     5
-binary_tree:            10
+binary_tree:            11
 --------------------------
 dynamic_programming:    14
 recursion_1:            2
@@ -22,5 +22,5 @@ queue_and_stack:        19
 recursion_2:            10
 trie:                   9
 
-Total:                  195
+Total:                  197
 """
