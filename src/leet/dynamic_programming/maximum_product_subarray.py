ad303f0ef45ec28165927ecbe7ec020a875f5c8f
zzhao30 <zzhao30@ford.com>
2022-06-16 15:51:39 -0400

    CCs.

--- src/leet/dynamic_programming/maximum_product_subarray.py
+++ src/leet/dynamic_programming/maximum_product_subarray.py
@@ -79,8 +79,9 @@ maxProduct(nums): O(1).
 def sol(nums):
     max_so_far = min_so_far = max_prod = nums[0]
     for num in nums[1:]:
-        max_temp = max(num, max_so_far * num, min_so_far * num)
-        min_so_far = min(num, max_so_far * num, min_so_far * num)
+        candidates = (num, max_so_far * num, min_so_far * num)
+        max_temp = max(candidates)
+        min_so_far = min(candidates)
         max_so_far = max_temp
         max_prod = max(max_prod, max_so_far)
     return max_prod
