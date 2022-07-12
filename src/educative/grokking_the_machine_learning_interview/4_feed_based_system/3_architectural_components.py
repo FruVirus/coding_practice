9952ef12dffeaada1114cd1ddb912d974ddeb9c4
zzhao30 <zzhao30@ford.com>
2022-07-12 11:44:45 -0400

    CCs.

---
+++ src/educative/grokking_the_machine_learning_interview/4_feed_based_system/3_architectural_components.py
@@ -0,0 +1,82 @@
+"""
+Main Takeaways
+~~~~~~~~~~~~~~
+
+User --> Generate Feed --> Tweet Selection --> Returned Candidates Tweets (I/O) -->
+[Ranker --> Feed (I/O) --> Feed Engagement Data --> Training Data Generation -->
+Ranker --> ...]
+
+The Tweet Selection component performs the first step in generating a user’s feed, i.e.,
+it fetches a pool of Tweets from the user’s network (the followees), since their last
+login. This pool of Tweets is then forwarded to the ranker component.
+
+The Ranker component will receive the pool of selected Tweets and predict their
+probability of engagement. The Tweets will then be ranked according to their predicted
+engagement probabilities for display on user A’s feed. If we zoom into this component,
+we can:
+
+    1. Train a single model to predict the overall engagement on the tweet.
+
+    2. Train separate models. Each model can focus on predicting the occurrence
+probability of a certain user action for the tweet. There will be a separate predictor
+for like, comment, time spent, share, hide, and report. The results of these models can
+be merged, each having a different weight/importance, to generate a rank score. The
+Tweets will then be ranked according to this score.
+
+Separately predicting each user action allows us to have greater control over the
+importance we want to give to each action when calculating the rank of the Tweet. We can
+tweak the weights to display Tweets in such a manner that would align with our current
+business objectives, i.e., give certain user actions higher/lower weight according to
+the business needs.
+
+Each user engagement action on the Twitter feed will generate positive and negative
+training examples for the user engagement prediction models.
+
+Architectural Components
+========================
+
+Architecture
+------------
+
+Let’s have a look at the architectural components that are integral in creating our
+Twitter feed system.
+
+User --> Generate Feed --> Tweet Selection --> Returned Candidates Tweets (I/O) -->
+[Ranker --> Feed (I/O) --> Feed Engagement Data --> Training Data Generation -->
+Ranker --> ...]
+
+Tweet selection
+---------------
+
+This component performs the first step in generating a user’s feed, i.e., it fetches a
+pool of Tweets from the user’s network (the followees), since their last login. This
+pool of Tweets is then forwarded to the ranker component.
+
+Ranker
+------
+
+The ranker component will receive the pool of selected Tweets and predict their
+probability of engagement. The Tweets will then be ranked according to their predicted
+engagement probabilities for display on user A’s feed. If we zoom into this component,
+we can:
+
+    1. Train a single model to predict the overall engagement on the tweet.
+
+    2. Train separate models. Each model can focus on predicting the occurrence
+probability of a certain user action for the tweet. There will be a separate predictor
+for like, comment, time spent, share, hide, and report. The results of these models can
+be merged, each having a different weight/importance, to generate a rank score. The
+Tweets will then be ranked according to this score.
+
+Separately predicting each user action allows us to have greater control over the
+importance we want to give to each action when calculating the rank of the Tweet. We can
+tweak the weights to display Tweets in such a manner that would align with our current
+business objectives, i.e., give certain user actions higher/lower weight according to
+the business needs.
+
+Training data generation
+------------------------
+
+Each user engagement action on the Twitter feed will generate positive and negative
+training examples for the user engagement prediction models.
+"""
