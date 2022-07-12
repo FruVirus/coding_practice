3b2d539a829de8a84662e3a331d5133511982026
zzhao30 <zzhao30@ford.com>
2022-07-11 22:37:43 -0400

    CCs.

---
+++ src/educative/grokking_the_machine_learning_interview/3_search_ranking/1_problem_statement.py
@@ -0,0 +1,33 @@
+"""
+Main Takeaways
+~~~~~~~~~~~~~~
+
+Clarify the problem statement by specifying three aspects: scope, scale, and
+personalization.
+
+Problem Statement
+=================
+
+Problem statement
+-----------------
+
+The interviewer has asked you to design a search relevance system for a search engine.
+
+Clarifying questions
+--------------------
+
+Let’s clarify the problem statement by specifying three aspects: scope, scale, and
+personalization.
+
+Problem scope
+
+
+
+Scale
+
+
+
+Personalization
+
+
+"""
