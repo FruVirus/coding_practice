5a3b473d9e8d454b1ba51d07bed9a15d846a67f5
FruVirus <zhen.zhao6@gmail.com>
2022-07-11 21:00:43 -0400

    CCs.

--- src/leet/arrays_101/container_with_most_water.py
+++ src/leet/arrays_101/container_with_most_water.py
@@ -57,7 +57,7 @@ def sol(height):
     while left < right:
         width = right - left
         maxarea = max(maxarea, min(height[left], height[right]) * width)
-        if height[left] <= height[right]:
+        if height[left] < height[right]:
             left += 1
         else:
             right -= 1
