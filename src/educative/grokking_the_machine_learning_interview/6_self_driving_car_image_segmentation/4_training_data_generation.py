73822b0f80f27c8f251535962ba9245070497be3
zzhao30 <zzhao30@ford.com>
2022-07-14 14:41:14 -0400

    CCs.

--- src/educative/grokking_the_machine_learning_interview/6_self_driving_car_image_segmentation/4_training_data_generation.py
+++ src/educative/grokking_the_machine_learning_interview/6_self_driving_car_image_segmentation/4_training_data_generation.py
@@ -2,158 +2,211 @@
 Main Takeaways
 ~~~~~~~~~~~~~~
 
-You will build your model on implicit feedback from the user. You will look at how a
-user interacts with media recommendations to generate positive and negative training
-examples.
+Human-labeled data and open source datasets are effective in generating manually labeled
+data. In most cases, your training data distribution will match with what you observe in
+the real-world scene images. However, you may not have enough data for all the
+conditions that you would like your model to make predictions for such as snow, rain,
+and night. For a self-autonomous vehicle to be perfect, your segmenter should work well
+for all the possible weather conditions, as well as cover a variety of obstacle images
+in the road.
 
-Generating training examples
-----------------------------
+It is an important area to think about how you can give your model training data for all
+conditions. One option is to manually label a lot of examples for each scenario. The
+second option is to use powerful data augmentation techniques to generate new training
+examples given your human-labeled data, especially for conditions that are missing in
+your training set.
 
-One way of interpreting user actions as positive and negative training examples is based
-on the duration for which the user watched a particular show/movie. You take positive
-examples as ones where the user ended up watching most of a recommended movie/show,
-i.e., watched 80% or more. You take negative examples, again where we are confident that
-the user ignored a movie/show, i.e., watched 10% or less.
+Training data enhancement through GANs
+--------------------------------------
 
-If the percentage of a movie/show watched by the user falls between 10% and 80%, you
-will put it in the uncertainty bucket. This percentage is not clearly indicative of a
-user’s like or dislike, so you ignore such examples. For instance, let’s say a user
-watched 55% of a movie. This could be considered a positive example considering that
-they liked it enough to watch it midway. However, it could be that a lot of people had
-recommended it to them, so they wanted to see what all the hype was about by at least
-watching it halfway through. However, they, ultimately, decided that it was not
-according to their liking.
+In the big picture, your self-driving system should compete with human intelligence when
+it comes to making decisions and planning movements. The segmenter can play its role in
+creating a reliable system by being able to accurately segment the driving scene in any
+condition that the vehicle experiences. Achieving this target requires a diverse set of
+training data that covers all the possible permutations of the driving scene. You need
+to devise a way to enhance your training data by converting snowy conditions of the
+ten-thousand Montreal images to sunny conditions and vice versa.
 
-Hence, to avoid these kinds of misinterpretations, you label examples as positive and
-negative only when you are certain about it to a higher degree.
+The target includes two parts:
 
-Balancing positive and negative training examples
--------------------------------------------------
+    1. Generating new training images
+    2. Ensuring generated images have different conditions (e.g., weather and lighting
+conditions).
 
-You have a lot more negative training examples than positive ones. To balance the ratio
-of positive and negative training samples, you can randomly downsample the negative
-examples.
+For the first part, you can utilize GANs. However, for the second part, i.e., to
+generate a different variation of the original segmented image, a simple GAN would not
+work. You would have to use conditional generative adversarial networks (cGANs) instead.
 
-Weighting training examples
----------------------------
+If you train a GAN on a set of driving images, you do not have any control over the
+generator’s output, i.e., it would just generate driving images similar to the images
+present in the training dataset. However, that is not particularly useful in your case.
 
-One way to incentivize your model to focus more on examples that have a higher
-contribution to the session watch time is to weight examples based on their contribution
-to session time. Here, you are assuming that your prediction model’s optimization
-function utilizes weight per example in its objective.
+Therefore, we want to perform image-to-image translation (a kind of data augmentation)
+to enhance our training data, i.e., you want to map features from an input image to an
+output image. For instance, you want to learn to map driving images with one weather
+condition to another weather condition, or you may want to convert day time driving
+images to night time images and vice versa. This can be done with a cGAN which is a deep
+neural network that extends the concept of GAN.
 
-One caveat of utilizing these weights is that the model might only recommend content
-with a longer watch time. So, it’s important to choose weights such that we are not
-solely focused on watch time. We should find the right balance between user satisfaction
-and watch time, based on our online A/B experiments.
+For image translation, your training data should consist of paired images. For example,
+image pairs could be of the same road during day and night. They could also be of the
+same road during winter and summer.
 
-Train test split
-----------------
+In cGAN, you give the source image that you want to translate to the generator, along
+with an additional input (target image as a condition). This will guide the generator’s
+output image to be of the same place but with a different weather/light condition.
 
-You need to be mindful of the fact that the user’s interaction patterns may differ
-throughout the week. Hence, you will use the interaction with recommendations throughout
-a week to capture all of the patterns during model training.
+You will also feed the source image to the discriminator so that apart from its usual
+task of discriminating between real and generated images, it will also see if the given
+image is a plausible translation of the source image.
 
-Random splitting defeats the purpose of training the model on a whole week’s data. Also,
-the data has a time dimension, i.e., you know the interaction on previous
-recommendations, and you want to predict the interaction with future recommendations.
-Hence, you will train the model on data from one time interval and validate it on the
-data from its succeeding time interval.
+You see that the cGAN loss function differs from the GAN loss function as the
+discriminator’s probability D(x) or D(G(z)) is now conditioned on y.
 
-Even though Netflix has millions of subscribers, on average they may watch a maximum of
-3 movies/1 show per week. Therefore, to generate a sufficient amount of data, you need
-to increase the time span over which you gather the data. Hence, we are using an entire
-month’s data to train your model.
+You can build multiple cGAN models for different conditions, including generating night
+images, cloudy images, snow images etc., and enhance our training data set with examples
+of all conditions.
+
+Targeted data gathering
+-----------------------
+
+Here, you aim to gather training data specifically on the areas where you fail while
+testing the system. It will help to further enhance the training data.
+
+You will test the self-driving car, with a person at the wheel. The vehicle has the
+capability to record the video of this test drive. After the test drive is over, you
+will observe the system and look for instances where it did not perform well and the
+person had to intervene. You will then focus on collecting such examples and ask the
+manual labelers to label them.
+
+After this, you can also apply data augmentation on the newly gathered training examples
+to further improve your model’s performance.
 
 Training Data Generation
 ========================
 
-You will build your model on implicit feedback from the user. You will look at how a
-user interacts with media recommendations to generate positive and negative training
-examples.
-
 Generating training examples
 ----------------------------
 
-One way of interpreting user actions as positive and negative training examples is based
-on the duration for which the user watched a particular show/movie. You take positive
-examples as ones where the user ended up watching most of a recommended movie/show,
-i.e., watched 80% or more. You take negative examples, again where we are confident that
-the user ignored a movie/show, i.e., watched 10% or less.
+Autonomous driving systems have to be extremely robust with no margin of error. This
+requires training each component model with all the possible scenarios that can happen
+in real life. Let’s see how to generate such training data for performing semantic
+segmentation.
 
-If the percentage of a movie/show watched by the user falls between 10% and 80%, you
-will put it in the uncertainty bucket. This percentage is not clearly indicative of a
-user’s like or dislike, so you ignore such examples. For instance, let’s say a user
-watched 55% of a movie. This could be considered a positive example considering that
-they liked it enough to watch it midway. However, it could be that a lot of people had
-recommended it to them, so they wanted to see what all the hype was about by at least
-watching it halfway through. However, they, ultimately, decided that it was not
-according to their liking.
+Human-labeled data
+------------------
 
-Hence, to avoid these kinds of misinterpretations, you label examples as positive and
-negative only when you are certain about it to a higher degree.
+First, you will have to gather driving images and hire people to label the images in a
+pixel-wise manner. There are many tools available such as Label Box that can facilitate
+the pixel-wise annotation of images.
 
-Balancing positive and negative training examples
--------------------------------------------------
+Open source datasets
+--------------------
 
-Each time a user logs in, Netflix provides a lot of recommendations. The user cannot
-watch all of them. Yes, people do binge-watch on Netflix, but still, this does not
-improve the positive to negative training examples ratio significantly. Therefore, you
-have a lot more negative training examples than positive ones. To balance the ratio of
-positive and negative training samples, you can randomly downsample the negative
-examples.
+There are quite a few open-source datasets available with segmented driving
+videos/images. One such example is the “BDD100K: A Large-scale Diverse Driving Video
+Database”. It contains segmented data for full frames.
 
-We balance the negative and positive examples to prevent classifier from favouring the
-outcome that has more examples.
+These two methods are effective in generating manually labeled data. In most cases, your
+training data distribution will match with what you observe in the real-world scene
+images. However, you may not have enough data for all the conditions that you would like
+your model to make predictions for such as snow, rain, and night. For a self-autonomous
+vehicle to be perfect, your segmenter should work well for all the possible weather
+conditions, as well as cover a variety of obstacle images in the road.
 
-Weighting training examples
----------------------------
+It is an important area to think about how you can give your model training data for all
+conditions. One option is to manually label a lot of examples for each scenario. The
+second option is to use powerful data augmentation techniques to generate new training
+examples given your human-labeled data, especially for conditions that are missing in
+your training set. Let’s discuss the second approach, using generative adversarial
+networks (GANs).
 
-Based on our training data discussion so far, all of the training examples are weighted
-equally, i.e., all have a weight of 1. According to Netflix’s business objectives, the
-main goal could be to increase the time a user spends on the platform.
+Training data enhancement through GANs
+--------------------------------------
 
-One way to incentivize your model to focus more on examples that have a higher
-contribution to the session watch time is to weight examples based on their contribution
-to session time. Here, you are assuming that your prediction model’s optimization
-function utilizes weight per example in its objective.
+In the big picture, your self-driving system should compete with human intelligence when
+it comes to making decisions and planning movements. The segmenter can play its role in
+creating a reliable system by being able to accurately segment the driving scene in any
+condition that the vehicle experiences.
 
-In the diagram above, you have two positive training examples. The first is a
-thirty-minute long show episode while the second is a two-hour long movie. The user
-watches 80% of both media. For the show, this equals twenty-four minutes, but for the
-movie, it means one hour and thirty-six minutes. You would assign more weight to the
-second example than the first one so that your model learns which kinds of media
-increases the session watch time.
+Achieving this target requires a diverse set of training data that covers all the
+possible permutations of the driving scene. For instance, assume that your dataset only
+contains ten-thousand driving images in the snowy Montreal conditions and fifty-thousand
+images in the sunny California conditions. You need to devise a way to enhance your
+training data by converting snowy conditions of the ten-thousand Montreal images to
+sunny conditions and vice versa.
 
-One caveat of utilizing these weights is that the model might only recommend content
-with a longer watch time. So, it’s important to choose weights such that we are not
-solely focused on watch time. We should find the right balance between user satisfaction
-and watch time, based on our online A/B experiments.
+The target includes two parts:
 
-Train test split
-----------------
+    1. Generating new training images
 
-Now, it’s time to split your training data for training and then testing your models.
-While doing so, you need to be mindful of the fact that the user’s interaction patterns
-may differ throughout the week. Hence, you will use the interaction with recommendations
-throughout a week to capture all of the patterns during model training.
+    2. Ensuring generated images have different conditions (e.g., weather and lighting
+conditions).
 
-You can randomly select 2/3 of the training data rows generated and utilize them for
-training purposes. The rest of the 1/3 can be used for validation and model testing.
+For the first part, you can utilize GANs. They are deep learning models used for
+generation of new content. However, for the second part, i.e., to generate a different
+variation of the original segmented image, a simple GAN would not work. You would have
+to use conditional generative adversarial networks (cGANs) instead. Let’s see why.
 
-However, this random splitting defeats the purpose of training the model on a whole
-week’s data. Also, the data has a time dimension, i.e., you know the interaction on
-previous recommendations, and you want to predict the interaction with future
-recommendations. Hence, you will train the model on data from one time interval and
-validate it on the data from its succeeding time interval. This will give you a more
-accurate picture of how your model will perform in a real scenario.
+If you train a GAN on a set of driving images, you do not have any control over the
+generator’s output, i.e., it would just generate driving images similar to the images
+present in the training dataset. However, that is not particularly useful in your case.
 
-In the following illustration, you are training the model using data generated from the
-month of June and using data generated in the first and second week of July for
-validation and testing purposes.
+Therefore, we want to perform image-to-image translation (a kind of data augmentation)
+to enhance our training data, i.e., you want to map features from an input image to an
+output image. For instance, you want to learn to map driving images with one weather
+condition to another weather condition, or you may want to convert day time driving
+images to night time images and vice versa. This can be done with a cGAN which is a deep
+neural network that extends the concept of GAN.
 
-Even though Netflix has millions of subscribers, on average they may watch a maximum of
-3 movies/1 show per week. Therefore, to generate a sufficient amount of data, you need
-to increase the time span over which you gather the data. Hence, we are using an entire
-month’s data to train your model.
+For image translation, your training data should consist of paired images. For example,
+image pairs could be of the same road during day and night. They could also be of the
+same road during winter and summer.
+
+In cGAN, you give the source image that you want to translate to the generator, along
+with an additional input (target image as a condition). This will guide the generator’s
+output image to be of the same place but with a different weather/light condition.
+
+You will also feed the source image to the discriminator so that apart from its usual
+task of discriminating between real and generated images, it will also see if the given
+image is a plausible translation of the source image.
+
+You see that the cGAN loss function differs from the GAN loss function as the
+discriminator’s probability D(x) or D(G(z)) is now conditioned on y.
+
+Once you have trained the cGAN for the image translation task, you can now feed it the
+driving images you manually collected through the generator model to enhance the
+training data.
+
+Using a similar approach, you can build multiple cGAN models for different conditions,
+including generating night images, cloudy images, snow images etc., and enhance our
+training data set with examples of all conditions.
+
+Targeted data gathering
+-----------------------
+
+Earlier in the chapter, we discussed manual intervention as a metric for our end-to-end
+self-driving car system. You can use this metric to identify scenarios where your
+segmentation model is not performing well and learn from them.
+
+Here, you aim to gather training data specifically on the areas where you fail while
+testing the system. It will help to further enhance the training data.
+
+You will test the self-driving car, with a person at the wheel. The vehicle has the
+capability to record the video of this test drive. After the test drive is over, you
+will observe the system and look for instances where it did not perform well and the
+person had to intervene.
+
+Consider a scenario where you observe that the person had to manually intervene when two
+pedestrians were jaywalking. You will check the performance of the segmentation model
+on, let’s say, the last twenty frames before the manual intervention took place. You
+might see that segmentation failed to predict one of the two pedestrians. You will
+select snapshots of those instances from the recording and ask the manual labelers to
+labels those snapshots since it means that the training data lacks such examples.
+
+You will then focus on collecting such examples and ask the manual labelers to label
+them.
+
+After this, you can also apply data augmentation on the newly gathered training examples
+to further improve your model’s performance.
 """
