2440c26abc620f81ff19a596177e485448c48696
zzhao30 <zzhao30@ford.com>
2022-07-20 16:45:23 -0400

    CCs.

---
+++ src/educative/grokking_the_system_design_interview/system_design_problems/2_designing_a_url_shortening_service_like_tinyurl.py
@@ -0,0 +1,11 @@
+"""
+Designing a URL Shortening service like TinyURL
+===============================================
+
+This service will provide short aliases redirecting to long URLs.
+
+1. Why do we need URL shortening?
+---------------------------------
+
+
+"""
