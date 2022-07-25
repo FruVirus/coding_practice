1ddf5f362bce0af27f56ad95b8c1fd52cdf6e299
zzhao30 <zzhao30@ford.com>
2022-07-25 11:20:11 -0400

    CCs.

--- src/leet/arrays_101/duplicate_zeros.py
+++ src/leet/arrays_101/duplicate_zeros.py
@@ -8,6 +8,28 @@ remaining elements to the right.
 Note that elements beyond the length of the original array are not written. Do the above
 modifications to the input array in place and do not return anything.
 
+Intuition
+---------
+
+1. Find the number of zeros which would be duplicated. We do need to make sure we are
+not counting the zeros which would be trimmed off since the discarded zeros won't be
+part of the final array. The number of zeros would give us the number of elements to be
+trimmed off the original array.
+
+2. Handle the edge case for a zero present on the boundary of the leftover elements.
+
+An example of the edge case is - [8,4,5,0,0,0,0,7]. In this array there is space to
+accommodate the duplicates of first and second occurrences of zero. But we don't have
+enough space for the duplicate of the third occurrence of zero. Hence when we are
+copying we need to make sure for the third occurrence we don't copy twice. Result =
+[8,4,5,0,0,0,0,0]
+
+3. Iterate the array from the end and copy a non-zero element once and zero element
+twice. When we say we discard the extraneous elements, it simply means we start from the
+left of the extraneous elements and start overwriting them with new values, eventually
+right shifting the left over elements and creating space for all the duplicated elements
+in the array.
+
 Complexity
 ==========
 
