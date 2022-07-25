672f5d2abf0a1348293a157edf334e853b88dcf2
zzhao30 <zzhao30@ford.com>
2022-07-25 12:09:24 -0400

    CCs.

--- src/leet/arrays_101/gas_station.py
+++ src/leet/arrays_101/gas_station.py
@@ -11,7 +11,7 @@ one of the gas stations.
 
 Given two integer arrays gas and cost, return the starting gas station's index if you
 can travel around the circuit once in the clockwise direction, otherwise return -1. If
-there exists a solution, it is guaranteed to be unique
+there exists a solution, it is guaranteed to be unique.
 
 Intuition
 ---------
