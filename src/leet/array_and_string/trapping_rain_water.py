2443fcbd1859d893e65d294797d0780f8cd45852
FruVirus <zhen.zhao6@gmail.com>
2022-07-31 10:57:30 -0400

    CCs.

--- src/leet/array_and_string/trapping_rain_water.py
+++ src/leet/array_and_string/trapping_rain_water.py
@@ -37,19 +37,19 @@ trap(height): O(1).
 
 
 def sol(height):
-    areas = max_l = max_r = 0
+    num_rain = max_l = max_r = 0
     l, r = 0, len(height) - 1
     while l < r:
         if height[l] < height[r]:
             if height[l] > max_l:
                 max_l = height[l]
             else:
-                areas += max_l - height[l]
+                num_rain += max_l - height[l]
             l += 1
         else:
             if height[r] > max_r:
                 max_r = height[r]
             else:
-                areas += max_r - height[r]
+                num_rain += max_r - height[r]
             r -= 1
-    return areas
+    return num_rain
