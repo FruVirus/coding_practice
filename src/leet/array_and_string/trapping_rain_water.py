7bad0ace238a1cef5101842b000d2225401c7b6f
FruVirus <zhen.zhao6@gmail.com>
2022-07-31 11:05:33 -0400

    CCs.

--- src/leet/array_and_string/trapping_rain_water.py
+++ src/leet/array_and_string/trapping_rain_water.py
@@ -12,14 +12,11 @@ For each element in the array, we find the maximum level of water it can trap af
 rain, which is equal to the minimum of maximum height of bars on both the sides minus
 its own height.
 
-Notice that as long as right_max[i] > left_max[i] (from element 0 to 6), the water
-trapped depends upon the left_max, and similar is the case when
-left_max[i] > right_max[i] (from element 8 to 11). So, we can say that if there is a
-larger bar at one end (say right), we are assured that the water trapped would be
-dependant on height of bar in current direction (from left to right). As soon as we find
-the bar at other end (right) is smaller, we start iterating in opposite direction (from
-right to left). We must maintain left_max and right_max during the iteration, but now we
-can do it in one iteration using 2 pointers, switching between the two.
+If height[left] < height[right], then for pointer left, he knows a taller bar exists on
+his right side, then if leftMax is taller than him, he can contain some water for sure.
+So we go ans += (left_max - height[left]). But if leftMax is shorter than him, then
+there isn't a left side bar can help him contain water, then he will become other bars'
+leftMax. so execute (left_max = height[left]). Same idea for right part.
 
 Complexity
 ==========
