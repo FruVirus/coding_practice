beff9e05b2799affeeb8f7fe22a01e9bb824730a
zzhao30 <zzhao30@ford.com>
2022-08-01 17:22:37 -0400

    CCs.

--- src/leet/graph/network_delay_time.py
+++ src/leet/graph/network_delay_time.py
@@ -11,6 +11,18 @@ We will send a signal from a given node k. Return the time it takes for all the 
 to receive the signal. If it is impossible for all the n nodes to receive the signal,
 return -1.
 
+Intuition
+---------
+
+Our objective is to find the fastest path from node k to every other node. This is a
+typical use case for the Single Source Shortest Path algorithm. Hence, In this approach,
+we will use Dijkstra's Algorithm to find the fastest path to every node from node k.
+
+We will start with node k and then iterate over every adjacent node neighborNode. In
+this approach, we will use a priority queue to traverse the nodes in increasing order of
+the time required to reach them. Therefore, in each iteration, we will visit the node
+with the shortest travel time. This will help us in finding the fastest time path first.
+
 Complexity
 ==========
 
@@ -31,7 +43,7 @@ networkDelayTime(times, n, k): O(n + e).
 import heapq
 
 
-def sol(times, n, k):
+def sol_dijkstra(times, n, k):
     graph = {i: [] for i in range(1, n + 1)}
     for u, v, w in times:
         graph[u].append((v, w))
