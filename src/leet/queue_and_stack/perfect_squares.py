96ca642acc10cef3376b111d01f2f0a79112ffb8
zzhao30 <zzhao30@ford.com>
2022-08-01 11:36:23 -0400

    CCs.

--- src/leet/queue_and_stack/perfect_squares.py
+++ src/leet/queue_and_stack/perfect_squares.py
@@ -8,6 +8,19 @@ A perfect square is an integer that is the square of an integer; in other words,
 the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect
 squares while 3 and 11 are not.
 
+Intuition
+---------
+
+1. First, for the square numbers that can be formed from n.
+2. We then create a queue variable which would keep all the remainders to enumerate at
+each level.
+3. In the main loop, we iterate over the queue variable. At each iteration, we check if
+the remainder is one of the square numbers. If the remainder is not a square number, we
+subtract it with one of the square numbers to obtain a new remainder and then add the
+new remainder to the next_queue for the iteration of the next level. We break out of the
+loop once we encounter a remainder that is of a square number, which also means that we
+find the solution.
+
 Complexity
 ==========
 
@@ -32,8 +45,7 @@ def sol(n):
             for square_num in square_nums:
                 if remainder == square_num:
                     return level
-                if remainder < square_num:
-                    break
-                next_queue.add(remainder - square_num)
+                if remainder >= square_num:
+                    next_queue.add(remainder - square_num)
         queue = next_queue
     return level
