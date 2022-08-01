81c28cbb0bc9bb56d66dc9281a0314a640325f19
zzhao30 <zzhao30@ford.com>
2022-08-01 10:10:59 -0400

    CCs.

--- src/leet/linked_list/reverse_linked_list_2.py
+++ src/leet/linked_list/reverse_linked_list_2.py
@@ -6,6 +6,17 @@ Given the head of a singly linked list and two integers left and right where
 left <= right, reverse the nodes of the list from position left to position right, and
 return the reversed list.
 
+Intuition
+---------
+
+1. Set a pointer to just before the sub-list (temp) to be reversed and a pointer to the
+first node in the sublist to be reversed (curr).
+2. Set a pointer to the last node of the sub-list before the reversed sub-list (conn)
+and a pointer to the tail of the (eventually) reversed sublist (tail).
+3. Reverse the sublist between left and right.
+4. Connect conn to the reversed sublist.
+5. Connect tail to the sublist after the reversed sublist.
+
 Complexity
 ==========
 
