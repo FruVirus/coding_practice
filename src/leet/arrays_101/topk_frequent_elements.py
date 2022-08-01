fb8e79335bcf15de35ddf39ef72643e3ff8145e0
FruVirus <zhen.zhao6@gmail.com>
2022-07-31 20:58:56 -0400

    CCs.

--- src/leet/arrays_101/topk_frequent_elements.py
+++ src/leet/arrays_101/topk_frequent_elements.py
@@ -28,6 +28,9 @@ Hence the array is now split into two parts. If by chance our pivot element took
 final position, then k elements on the right are these top k frequent we're looking for.
 If not, we can choose one more pivot and place it in its perfect position.
 
+1. Build a hash map element -> its frequency and convert its keys into the array unique
+of unique elements. Note that elements are unique, but their frequencies are not. That
+means we need a partition algorithm that works fine with duplicates.
 
 Complexity
 ==========
@@ -54,3 +57,30 @@ def sol_one(nums, k):
         b[freq - 1].append(num)
     flat = [item for sublist in b for item in sublist]
     return flat[len(flat) - k :]
+
+
+def sol_two(nums, k):
+    count = Counter(nums)
+    unique = list(count.keys())
+    n = len(unique)
+
+    def partition(low, high, pivot_index) -> int:
+        pivot_freq, i = count[unique[pivot_index]], low - 1
+        unique[high], unique[pivot_index] = unique[pivot_index], unique[high]
+        for j in range(low, high):
+            if count[unique[j]] <= pivot_freq:
+                i += 1
+                unique[i], unique[j] = unique[j], unique[i]
+        unique[high], unique[i + 1] = unique[i + 1], unique[high]
+        return i + 1
+
+    def quickselect(low, high, i) -> None:
+        pivot = partition(low, high, random.randrange(low, high + 1))
+        if i == pivot:
+            return
+        if i < pivot:
+            return quickselect(low, pivot - 1, i)
+        return quickselect(pivot + 1, high, i)
+
+    quickselect(0, n - 1, n - k)
+    return unique[n - k:]
