1a199d5ac21a318465d6b5600e1751ecd0a1ba40
zzhao30 <zzhao30@ford.com>
2022-08-01 09:55:29 -0400

    CCs.

--- src/leet/linked_list/reorder_list.py
+++ src/leet/linked_list/reorder_list.py
@@ -12,6 +12,13 @@ L_0 --> L_n --> L_1 → L_(n - 1) --> L_2 --> L_(n - 2) --> ...
 
 You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
+Intuition
+---------
+
+1. Get the midpoint of the linked list.
+2. Reverse the second half of the linked list.
+3. Merge the first and second halves one element at a time.
+
 Complexity
 ==========
 
@@ -30,10 +37,10 @@ reorderList(head): O(1).
 def sol(head):
     if not head:
         return None
-    slow = fast = head
+    mid = fast = head
     while fast and fast.next:
-        slow, fast = slow.next, fast.next.next
-    temp, curr = None, slow
+        mid, fast = mid.next, fast.next.next
+    temp, curr = None, mid
     while curr:
         next, curr.next = curr.next, temp
         curr, temp = next, curr
